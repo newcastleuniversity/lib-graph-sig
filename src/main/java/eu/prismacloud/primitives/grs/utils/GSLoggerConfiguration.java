caa86352c111de14891884499bab13b4320929b8
Ioannis Sfyrakis <i.sfyrakis@ncl.ac.uk>
2017-12-26 03:39:59 +0200

    add gs logger configuration class
    add logging.properties file for configuring logging levels

---
+++ src/main/java/eu/prismacloud/primitives/grs/utils/GSLoggerConfiguration.java
@@ -0,0 +1,32 @@
+package eu.prismacloud.primitives.grs.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+/**
+ * Configure Java logger 
+ */
+public class GSLoggerConfiguration {
+
+    private static Logger gslog = null;
+
+    static {
+        InputStream stream = GSLoggerConfiguration.class.getClassLoader().
+                getResourceAsStream("logging.properties");
+        try {
+            
+            LogManager.getLogManager().readConfiguration(stream);
+            gslog = Logger.getLogger(GSLoggerConfiguration.class.getName());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Logger getGSlog() {
+        return gslog;
+    }
+}
