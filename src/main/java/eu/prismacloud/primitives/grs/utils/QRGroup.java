7ea8945a7c4a1a44442ffb36c2315216b6deaae7
Ioannis Sfyrakis <i.sfyrakis@ncl.ac.uk>
2017-12-27 20:04:18 +0200

    update javadoc
    delegate isElementOfQR method to facade

--- src/main/java/eu/prismacloud/primitives/grs/utils/QRGroup.java
+++ src/main/java/eu/prismacloud/primitives/grs/utils/QRGroup.java
@@ -11,11 +11,15 @@ public final class QRGroup extends Group {
     private final BigInteger pPrime;
     private final BigInteger qPrime;
     private final BigInteger order;
+    private final BigInteger generator = BigInteger.ONE;
 
-    private BigInteger generator;
-
-
-    public QRGroup(BigInteger pPrime, BigInteger qPrime) {
+    /**
+     * Instantiates a new QR group.
+     *
+     * @param pPrime the p prime
+     * @param qPrime the q prime
+     */
+    public QRGroup(final BigInteger pPrime, final BigInteger qPrime) {
 
         this.modulus = pPrime.multiply(qPrime);
         this.pPrime = pPrime;
@@ -23,7 +27,6 @@ public final class QRGroup extends Group {
         this.order = getOrder();
 
     }
-
 
     @Override
     public BigInteger getOrder() {
@@ -41,6 +44,11 @@ public final class QRGroup extends Group {
         return this.generator;
     }
 
+    /**
+     * Create generator group element for QRN.
+     *
+     * @return the group element
+     */
     public GroupElement createGenerator() {
         return new QRElement(this, CryptoUtilsFacade.computeQRNGenerator(this.modulus));
     }
@@ -48,16 +56,17 @@ public final class QRGroup extends Group {
     /**
      * Algorithm <tt>alg:element_of_QR_N</tt> - topocert-doc
      * Determines if an integer  alpha is an element of QRN
-     * 
+     * If the factorization of a Special RSA modulus is known
+     * \(N = pq\) then \((a | p) = 1 \land (a | q) = 1\)
+     *
      * @param alpha candidate integer alpha,
-     * @return  true if alpha in QRN, false if alpha not in QRN
+     * @return true if alpha in QRN, false if alpha not in QRN
      * Dependencies: jacobiSymbol()
      */
     @Override
     public boolean isElement(BigInteger alpha) {
-        // TODO check if it is correct
-        return alpha.compareTo(BigInteger.ZERO) > 0 && alpha.compareTo(modulus.subtract(BigInteger.ONE).divide(NumberConstants.TWO.getValue())) <= 0
-                && JacobiSymbol.computeJacobiSymbol(alpha, pPrime) == 1 && JacobiSymbol.computeJacobiSymbol(alpha, qPrime) == 1;
+        // TODO check if computations are correct
+        return CryptoUtilsFacade.isElementOfQR(alpha, pPrime) && CryptoUtilsFacade.isElementOfQR(alpha, qPrime);
 
     }
 }
