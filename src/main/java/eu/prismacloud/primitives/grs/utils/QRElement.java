722c4934cd7975a565df51a637c0bc0baf667bd2
Ioannis Sfyrakis <i.sfyrakis@ncl.ac.uk>
2017-12-27 02:29:11 +0200

    update qrelement
    refactor names

--- src/main/java/eu/prismacloud/primitives/grs/utils/QRElement.java
+++ src/main/java/eu/prismacloud/primitives/grs/utils/QRElement.java
@@ -5,131 +5,134 @@ import java.math.BigInteger;
 /**
  * element of Quadratic Residue
  */
-public class QRElement {
+public class QRElement extends GroupElement{
     private QRGroup group;
 
 
-    private final BigInteger representation;
+    private final BigInteger value;
 
-    public QRElement(BigInteger representation) {
-        this.representation = representation;
+    public QRElement(BigInteger value) {
+        this.value = value;
     }
 
-    public QRElement(final QRGroup group, final BigInteger representation) {
+    public QRElement(final QRGroup group, final BigInteger value) {
 
         this.group = group;
-        this.representation = representation;
+        this.value = value;
+        
     }
 
-    public BigInteger getRepresentation() {
-        return representation;
+    public BigInteger getValue() {
+        return value;
     }
 
     public BigInteger nextProbablePrime() {
-        return representation.nextProbablePrime();
+        return value.nextProbablePrime();
     }
 
     public BigInteger add(BigInteger val) {
-        return representation.add(val);
+        return value.add(val);
     }
 
     public BigInteger subtract(BigInteger val) {
-        return representation.subtract(val);
+        return value.subtract(val);
     }
 
     public BigInteger multiply(BigInteger val) {
-        return representation.multiply(val);
+        return value.multiply(val);
     }
 
     public BigInteger divide(BigInteger val) {
-        return representation.divide(val);
+        return value.divide(val);
     }
 
     public BigInteger[] divideAndRemainder(BigInteger val) {
-        return representation.divideAndRemainder(val);
+        return value.divideAndRemainder(val);
     }
 
     public BigInteger remainder(BigInteger val) {
-        return representation.remainder(val);
+        return value.remainder(val);
     }
 
     public BigInteger gcd(BigInteger val) {
-        return representation.gcd(val);
+        return value.gcd(val);
     }
 
     public BigInteger abs() {
-        return representation.abs();
+        return value.abs();
     }
 
     public BigInteger negate() {
-        return representation.negate();
+        return value.negate();
     }
 
     public int signum() {
-        return representation.signum();
+        return value.signum();
     }
 
     public BigInteger mod(BigInteger m) {
-        return representation.mod(m);
+        return value.mod(m);
     }
 
     public BigInteger modPow(BigInteger exponent, BigInteger m) {
-        return representation.modPow(exponent, m);
+        return value.modPow(exponent, m);
     }
 
     public BigInteger modInverse(BigInteger m) {
-        return representation.modInverse(m);
+        return value.modInverse(m);
     }
 
     public int getLowestSetBit() {
-        return representation.getLowestSetBit();
+        return value.getLowestSetBit();
     }
 
     public int bitLength() {
-        return representation.bitLength();
+        return value.bitLength();
     }
 
     public int bitCount() {
-        return representation.bitCount();
+        return value.bitCount();
     }
 
     public boolean isProbablePrime(int certainty) {
-        return representation.isProbablePrime(certainty);
+        return value.isProbablePrime(certainty);
     }
 
     public int compareTo(BigInteger val) {
-        return representation.compareTo(val);
+        return value.compareTo(val);
     }
 
     public boolean equals(Object x) {
-        return representation.equals(x);
+        return value.equals(x);
     }
 
     public BigInteger min(BigInteger val) {
-        return representation.min(val);
+        return value.min(val);
     }
 
     public BigInteger max(BigInteger val) {
-        return representation.max(val);
+        return value.max(val);
     }
 
     public int hashCode() {
-        return representation.hashCode();
+        return value.hashCode();
     }
 
     public String toString(int radix) {
-        return representation.toString(radix);
+        return value.toString(radix);
     }
 
     public String toString() {
-        return representation.toString();
+        return value.toString();
     }
 
     public byte[] toByteArray() {
-        return representation.toByteArray();
+        return value.toByteArray();
     }
 
     public QRGroup getGroup() {
         return group;
     }
+
+   
 }
