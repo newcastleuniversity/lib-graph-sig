2baf45a5052ace9e5ba470f20a1ffe5d78189c4f
Ioannis Sfyrakis <i.sfyrakis@ncl.ac.uk>
2017-12-27 02:36:21 +0200

    cleanup SafePrime class

--- src/main/java/eu/prismacloud/primitives/grs/utils/SafePrime.java
+++ src/main/java/eu/prismacloud/primitives/grs/utils/SafePrime.java
@@ -1,8 +1,5 @@
 package eu.prismacloud.primitives.grs.utils;
 
-import com.ibm.zurich.idmx.utils.Utils;
-import eu.prismacloud.primitives.grs.parameters.KeyGenParameters;
-
 import java.math.BigInteger;
 import java.util.logging.Logger;
 
@@ -12,15 +9,16 @@ import java.util.logging.Logger;
 public class SafePrime {
 
     private BigInteger a;
-    SafePrime sf;
-    BigInteger a_prime;
+    private SafePrime sf;
+    private BigInteger a_prime;
 
     private static final Logger log = Logger.getLogger(SafePrime.class.getName());
 
     /**
      * Instantiates a new Safe prime p with its corresponding Sophie Germain prime p'.
      * \( p = 2p' + 1 \)
-     * @param safePrime     the safe prime 
+     *
+     * @param safePrime     the safe prime
      * @param sophieGermain the Sophie Germain prime
      */
     public SafePrime(final BigInteger safePrime, final BigInteger sophieGermain) {
@@ -48,12 +46,7 @@ public class SafePrime {
     public SafePrime generateRandomSafePrime() {
         CryptoUtilsFacade cuf = new CryptoUtilsFacade();
         sf = cuf.computeRandomSafePrime();
-//        a_prime =  a.asubtract(ONE).shiftRight(1);
         return new SafePrime(sf.a, sf.a_prime);
     }
-
-
-
-
 
 }
