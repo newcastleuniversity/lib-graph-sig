a890ad831046b2ad7c8d572cbf8bb59b718ce5bb
Ioannis Sfyrakis <i.sfyrakis@ncl.ac.uk>
2018-02-02 15:29:31 +0000

    improve test for public key

--- src/test/java/eu/prismacloud/primitives/grs/keys/SignerPublicKeyTest.java
+++ src/test/java/eu/prismacloud/primitives/grs/keys/SignerPublicKeyTest.java
@@ -4,17 +4,20 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.logging.Logger;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Created by Ioannis Sfyrakis on 02/02/2018
- */
+
 class SignerPublicKeyTest {
+    private static final Logger log = Logger.getLogger(SignerPublicKeyTest.class.getName());
+       private GSSignerKeyPair gsk;
 
-    @BeforeEach
-    void setUp() {
-    }
-
+       @BeforeEach
+       void setUp() {
+          // classUnderTest = new GSSignerKeyPair();
+            gsk = GSSignerKeyPair.KeyGen();
+       }
     @AfterEach
     void tearDown() {
     }
