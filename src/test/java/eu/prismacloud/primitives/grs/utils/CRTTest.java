d90f2098931877a0f0b0171d8faae3c6a0f7cd73
Ioannis Sfyrakis <i.sfyrakis@ncl.ac.uk>
2018-02-08 00:56:58 +0000

    update tests for CRT algo

--- src/test/java/eu/prismacloud/primitives/grs/utils/CRTTest.java
+++ src/test/java/eu/prismacloud/primitives/grs/utils/CRTTest.java
@@ -27,14 +27,31 @@ class CRTTest {
     @BeforeEach
     void setUp() {
         /**
-         * \( x \equiv 2 \bmod 3 \)
-         * \( x \equiv 3 \bmod 5 \)
+         * \( x_p \equiv 1 \bmod 5 \)
+         * \( x_q \equiv 2 \bmod 3 \)
          */
-        a = BigInteger.valueOf(2);
-        p = BigInteger.valueOf(3);
-        b = BigInteger.valueOf(3);
-        q = BigInteger.valueOf(5);
+        a = BigInteger.valueOf(1);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(2);
+        q = BigInteger.valueOf(3);
         x = BigInteger.valueOf(8);
+    }
+
+    @Test
+    @DisplayName("Test Chinese Remainder Theorem")
+    void computeCRTPC() {
+        log.info("@Test: computeCRT");
+
+        EEAlgorithm.computeEEAlgorithm(p, q);
+        log.info("crt s: " + EEAlgorithm.getS());
+        log.info("crt t: " + EEAlgorithm.getT());
+        log.info("crt modInverse: " + p.modInverse(q));
+
+        BigInteger res = CRT.computeCRT(a, BigInteger.valueOf(6), b, BigInteger.valueOf(10), p.multiply(q));
+        log.info("result: " + res);
+
+        assertEquals(BigInteger.valueOf(11), res);
+
     }
 
     @Test
@@ -50,31 +67,129 @@ class CRTTest {
         BigInteger res = CRT.computeCRT(a, p, b, q);
         log.info("result: " + res);
 
-        assertEquals(BigInteger.valueOf(8), res);
+        assertEquals(BigInteger.valueOf(11), res);
 
     }
 
     @Test
-    @DisplayName("Test convert to modulo N representation ")
-    void convertToModuloN() {
+    @DisplayName("Test Chinese Remainder Theorem in Z star 15")
+    void testCRTZStar15() {
+        log.info("@Test: testCRTZStar15");
+        BigInteger result;
+        // test CRT in \( Z^*_15 = { 1, 2, 4, 7, 8, 11, 13, 14} \)
 
-        EEAlgorithm.computeEEAlgorithm(p, q);
-        log.info("crt s: " + EEAlgorithm.getS());
-        log.info("crt t: " + EEAlgorithm.getT());
-        log.info("crt modInverse: " + p.modInverse(q));
+        // test 1 <-> (1,1)
+        a = BigInteger.valueOf(1);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(1);
+        q = BigInteger.valueOf(3);
+        x = BigInteger.valueOf(1);
 
-        BigInteger res = CRT.convertToModuloN(a, p, b, q);
-        log.info("result: " + res);
+        result = CRT.computeCRT(a, p, b, q);
 
-        assertEquals(BigInteger.valueOf(8), res);
 
+        log.info("result 1 <-> (1,1) " + result);
+        assertEquals(x, result);
+
+
+        // test 2 <-> (2,2)
+        a = BigInteger.valueOf(2);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(2);
+        q = BigInteger.valueOf(3);
+        x = BigInteger.valueOf(2);
+
+        result = CRT.computeCRT(a, p, b, q);
+
+        log.info("result 2 <-> (2,2) " + result);
+        assertEquals(x, result);
+
+
+        // test 4 <-> (4,1)
+        a = BigInteger.valueOf(4);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(1);
+        q = BigInteger.valueOf(3);
+        x = BigInteger.valueOf(4);
+
+        result = CRT.computeCRT(a, p, b, q);
+
+        log.info("result 4 <-> (4,1) " + result);
+        assertEquals(x, result);
+
+
+        // test 7 <-> (2,1)
+        a = BigInteger.valueOf(2);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(1);
+        q = BigInteger.valueOf(3);
+        x = BigInteger.valueOf(7);
+
+        result = CRT.computeCRT(a, p, b, q);
+
+        log.info("result 7 <-> (2,1) " + result);
+        assertEquals(x, result);
+
+        // test 8 <-> (3,2)
+        a = BigInteger.valueOf(3);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(2);
+        q = BigInteger.valueOf(3);
+        x = BigInteger.valueOf(8);
+
+        result = CRT.computeCRT(a, p, b, q);
+
+        log.info("result 8 <-> (3,2) " + result);
+        assertEquals(x, result);
+
+        // test 11 <-> (1,2)
+        a = BigInteger.valueOf(1);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(2);
+        q = BigInteger.valueOf(3);
+        x = BigInteger.valueOf(11);
+
+        result = CRT.computeCRT(a, p, b, q);
+
+        log.info("result 11 <-> (1,2) " + result);
+        assertEquals(x, result);
+
+
+        // test 13 <-> (3,1)
+        a = BigInteger.valueOf(3);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(1);
+        q = BigInteger.valueOf(3);
+        x = BigInteger.valueOf(13);
+
+        result = CRT.computeCRT(a, p, b, q);
+
+        log.info("result 13 <-> (3,1) " + result);
+        assertEquals(x, result);
+
+        // test 14 <-> (4,2)
+        a = BigInteger.valueOf(4);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(2);
+        q = BigInteger.valueOf(3);
+        x = BigInteger.valueOf(14);
+
+        result = CRT.computeCRT(a, p, b, q);
+
+        log.info("result 14 <-> (4,2) " + result);
+        assertEquals(x, result);
 
     }
+
 
     @Test
     @DisplayName("Test convert to pq representation ")
     void convertToPQ() {
-
+        a = BigInteger.valueOf(1);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(2);
+        q = BigInteger.valueOf(3);
+        x = BigInteger.valueOf(11);
         EEAlgorithm.computeEEAlgorithm(p, q);
         log.info("crt s: " + EEAlgorithm.getS());
         log.info("crt t: " + EEAlgorithm.getT());
@@ -87,5 +202,42 @@ class CRTTest {
         assertEquals(a, qr.getXp());
         assertEquals(b, qr.getXq());
 
+    }
+
+    @Test
+    void compute1p() {
+        a = BigInteger.valueOf(1);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(2);
+        q = BigInteger.valueOf(3);
+        x = BigInteger.valueOf(8);
+
+        EEAlgorithm.computeEEAlgorithm(p, q);
+        log.info("crt s: " + EEAlgorithm.getS());
+        log.info("crt t: " + EEAlgorithm.getT());
+        log.info("crt modInverse: " + p.modInverse(q));
+        BigInteger Y = EEAlgorithm.getT();
+        BigInteger one_p = CRT.compute1p(Y, p, q);
+        log.info("one_p: " + one_p);
+        assertEquals(BigInteger.valueOf(6), one_p);
+
+    }
+
+    @Test
+    void compute1q() {
+        a = BigInteger.valueOf(1);
+        p = BigInteger.valueOf(5);
+        b = BigInteger.valueOf(2);
+        q = BigInteger.valueOf(3);
+        x = BigInteger.valueOf(8);
+
+        EEAlgorithm.computeEEAlgorithm(p, q);
+        log.info("crt s: " + EEAlgorithm.getS());
+        log.info("crt t: " + EEAlgorithm.getT());
+        log.info("crt modInverse: " + p.modInverse(q));
+        BigInteger X = EEAlgorithm.getS();
+        BigInteger one_q = CRT.compute1q(X, p, q);
+        log.info("one_q: " + one_q);
+        assertEquals(BigInteger.valueOf(10), one_q);
     }
 }
