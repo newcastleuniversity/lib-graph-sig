d45562c209c54209d3da0c992423c534e38e9745
Ioannis Sfyrakis <i.sfyrakis@ncl.ac.uk>
2018-02-16 13:19:11 +0000

    add abstract methods for Groups

--- src/main/java/eu/prismacloud/primitives/grs/utils/Group.java
+++ src/main/java/eu/prismacloud/primitives/grs/utils/Group.java
@@ -13,4 +13,10 @@ public abstract class Group {
     public abstract BigInteger getModulus();
 //    public abstract GroupElement createGenerator(BigInteger rho, BigInteger gamma);
     public abstract boolean  isElement(BigInteger value);
+
+    public abstract GroupElement createGenerator();
+
+    public abstract GroupElement createElement();
+
+    public abstract GroupElement createElement(GroupElement s);
 }
