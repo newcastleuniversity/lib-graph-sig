084a7cf85c5f7dda1e86e8336a51f3f3429d551a
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-05-21 01:11:23 +0100

    update code style

--- src/main/java/eu/prismacloud/primitives/grs/commitment/ICommitment.java
+++ src/main/java/eu/prismacloud/primitives/grs/commitment/ICommitment.java
@@ -3,13 +3,13 @@ package eu.prismacloud.primitives.grs.commitment;
 import java.math.BigInteger;
 
 public interface ICommitment {
-    BigInteger getCommitment();
+  BigInteger getCommitment();
 
-//    BigInteger getCapR();
-//
-//    BigInteger getCapS();
-//
-//    BigInteger getN();
-//
-//    int getNumBases();
+  //    BigInteger getCapR();
+  //
+  //    BigInteger getCapS();
+  //
+  //    BigInteger getN();
+  //
+  //    int getNumBases();
 }
