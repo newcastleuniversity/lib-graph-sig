ad3496c8d2af93248909d2ffa8ae8f24a4d3adcf
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-05-21 01:07:53 +0100

    update code style

--- src/main/java/eu/prismacloud/primitives/grs/utils/IdemixUtils.java
+++ src/main/java/eu/prismacloud/primitives/grs/utils/IdemixUtils.java
@@ -5,87 +5,100 @@ import com.ibm.zurich.idmx.utils.StructureStore;
 import com.ibm.zurich.idmx.utils.SystemParameters;
 import com.ibm.zurich.idmx.utils.Utils;
 import eu.prismacloud.primitives.grs.parameters.KeyGenParameters;
-
 import java.math.BigInteger;
 import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
- * Wrapper for Utils class in IDEMIX  library
+ * Wrapper for Utils class in IDEMIX library
+ *
+ * @see eu.prismacloud.primitives.grs.utils.INumberUtils
  */
 public class IdemixUtils extends Utils implements INumberUtils {
 
-    private Utils utils;
-    private SystemParameters sp = null;
-    private GroupParameters gp;
+  private Utils utils;
+  private SystemParameters sp = null;
+  private GroupParameters gp;
 
-    /**
-     * Instantiates a new Idemix utils.
-     */
-    public IdemixUtils() {
-        super();
+  /** Instantiates a new Idemix utils. */
+  public IdemixUtils() {
+    super();
+  }
+
+  @Override
+  public SafePrime generateRandomSafePrime() {
+    BigInteger p =
+        Utils.computeSafePrime(
+            KeyGenParameters.l_n.getValue() / 2, KeyGenParameters.l_pt.getValue());
+    BigInteger p_prime = p.subtract(BigInteger.ONE).shiftRight(1);
+
+    return new SafePrime(p, p_prime);
+  }
+
+  @Override
+  public SpecialRSAMod generateSpecialRSAModulus() {
+    return null;
+  }
+
+  @Override
+  public BigInteger createQRNGenerator(BigInteger n) {
+    return Utils.computeGeneratorQuadraticResidue(n, getIdemixSystemParameters());
+  }
+
+  @Override
+  public BigInteger createRandomNumber(final BigInteger lowerBound, final BigInteger upperBound) {
+    return Utils.computeRandomNumber(lowerBound, upperBound, this.getIdemixSystemParameters());
+  }
+
+  @Override
+  public CommitmentGroup generateCommitmentGroup() {
+    StructureStore st = StructureStore.getInstance();
+    st.add("idemix", this.getIdemixSystemParameters());
+
+    try {
+      gp = GroupParameters.generateGroupParams(new URI("idemix"));
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
     }
 
-    @Override
-    public SafePrime generateRandomSafePrime() {
-        BigInteger p = Utils.computeSafePrime(KeyGenParameters.l_n.getValue() / 2, KeyGenParameters.l_pt.getValue());
-        BigInteger p_prime = p.subtract(BigInteger.ONE).shiftRight(1);
+    return new CommitmentGroup(gp.getRho(), gp.getCapGamma(), gp.getG(), gp.getH());
+  }
 
-        return new SafePrime(p, p_prime);
+  @Override
+  public BigInteger createCommitmentGroupGenerator(BigInteger rho, BigInteger gamma) {
+    return GroupParameters.newGenerator(rho, gamma, getIdemixSystemParameters());
+  }
 
-    }
+  @Override
+  public Boolean elementOfQRN(BigInteger value, BigInteger modulus) {
+    throw new RuntimeException("not implemented in idemix library");
+  }
 
-    @Override
-    public SpecialRSAMod generateSpecialRSAModulus() {
-        return null;
-    }
+  @Override
+  public BigInteger createQRNElement(BigInteger n) {
+    throw new RuntimeException("not implemented in idemix library");
+  }
 
-    @Override
-    public BigInteger createQRNGenerator(BigInteger n) {
-        return Utils.computeGeneratorQuadraticResidue(n, getIdemixSystemParameters());
-    }
+  private SystemParameters getIdemixSystemParameters() {
 
-
-    @Override
-    public BigInteger createRandomNumber(final BigInteger lowerBound, final BigInteger upperBound) {
-        return Utils.computeRandomNumber(lowerBound, upperBound, this.getIdemixSystemParameters());
-    }
-
-    @Override
-    public CommitmentGroup generateCommitmentGroup() {
-        StructureStore st = StructureStore.getInstance();
-        st.add("idemix", this.getIdemixSystemParameters());
-
-        try {
-            gp = GroupParameters.generateGroupParams(new URI("idemix"));
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        
-        return new CommitmentGroup(gp.getRho(), gp.getCapGamma(), gp.getG(), gp.getH());
-    }
-
-    @Override
-    public BigInteger createCommitmentGroupGenerator(BigInteger rho, BigInteger gamma) {
-        return GroupParameters.newGenerator(rho, gamma, getIdemixSystemParameters());
-    }
-
-    @Override
-    public Boolean elementOfQRN(BigInteger value, BigInteger modulus) {
-        throw new RuntimeException("not implemented in idemix library");
-    }
-
-    @Override
-    public BigInteger createQRNElement(BigInteger n) {
-        throw new RuntimeException("not implemented in idemix library");
-    }
-
-    private SystemParameters getIdemixSystemParameters() {
-
-        if (sp == null) {
-            sp = new SystemParameters(KeyGenParameters.l_e.getValue(), KeyGenParameters.l_prime_e.getValue(), KeyGenParameters.l_gamma.getValue(), KeyGenParameters.l_H.getValue(), 0, KeyGenParameters.l_m.getValue(), KeyGenParameters.l_n.getValue(), KeyGenParameters.l_0.getValue(), KeyGenParameters.l_pt.getValue(), KeyGenParameters.l_r.getValue(), KeyGenParameters.l_res.getValue(), KeyGenParameters.l_rho.getValue(), KeyGenParameters.l_v.getValue(), 0);
-            return sp;
-        } else return this.sp;
-
-    }
+    if (sp == null) {
+      sp =
+          new SystemParameters(
+              KeyGenParameters.l_e.getValue(),
+              KeyGenParameters.l_prime_e.getValue(),
+              KeyGenParameters.l_gamma.getValue(),
+              KeyGenParameters.l_H.getValue(),
+              0,
+              KeyGenParameters.l_m.getValue(),
+              KeyGenParameters.l_n.getValue(),
+              KeyGenParameters.l_0.getValue(),
+              KeyGenParameters.l_pt.getValue(),
+              KeyGenParameters.l_r.getValue(),
+              KeyGenParameters.l_res.getValue(),
+              KeyGenParameters.l_rho.getValue(),
+              KeyGenParameters.l_v.getValue(),
+              0);
+      return sp;
+    } else return this.sp;
+  }
 }
