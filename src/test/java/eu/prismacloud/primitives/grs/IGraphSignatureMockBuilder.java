91689f7fc837c89f35ead604762dc0647fb95e04
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-05-21 01:06:14 +0100

    update code style

--- src/test/java/eu/prismacloud/primitives/grs/IGraphSignatureMockBuilder.java
+++ src/test/java/eu/prismacloud/primitives/grs/IGraphSignatureMockBuilder.java
@@ -2,36 +2,39 @@ package eu.prismacloud.primitives.grs;
 
 import eu.prismacloud.primitives.grs.signature.IGraphSignature;
 
-
 public class IGraphSignatureMockBuilder {
-    private IGraphSignature mock;
+  private IGraphSignature mock;
 
-//    public IGraphSignatureMockBuilder() {
-//        mock = EasyMock.mock(IGraphSignature.class);
-//    }
-//
-//    public IGraphSignatureMockBuilder keyGen(int securityParam, IKeyGenParams params, final IKeyGenPair expected) {
-//        EasyMock.expect(mock.keyGen(securityParam, params)).andReturn(expected).once();
-//        return this;
-//    }
-//
-//    public IGraphSignatureMockBuilder commit(IGraphRepresentation graph, BigInteger rnd, final ICommitment expected) {
-//        EasyMock.expect(mock.commit(graph, rnd)).andReturn(expected).once();
-//        return this;
-//    }
-//
-//    public IGraphSignatureMockBuilder hiddenSign(ICommitment cmt, ISignerPublicKey pk_s, final IGraphSignature expected) {
-//        EasyMock.expect(mock.hiddenSign(cmt, pk_s)).andReturn(expected).once();
-//        return this;
-//    }
-//
-//    public IGraphSignatureMockBuilder verify(ISignerPublicKey pk_s, ICommitment cmt, BigInteger rTilde, IGraphSignature gsig, final Boolean expected) {
-//        EasyMock.expect(mock.verify(pk_s, cmt, rTilde, gsig)).andReturn(expected).once();
-//        return this;
-//    }
-//
-//    public IGraphSignature buildAndReplay() {
-//        EasyMock.replay(mock);
-//        return mock;
-//    }
+  //    public IGraphSignatureMockBuilder() {
+  //        mock = EasyMock.mock(IGraphSignature.class);
+  //    }
+  //
+  //    public IGraphSignatureMockBuilder keyGen(int securityParam, IKeyGenParams params, final
+  // IKeyGenPair expected) {
+  //        EasyMock.expect(mock.keyGen(securityParam, params)).andReturn(expected).once();
+  //        return this;
+  //    }
+  //
+  //    public IGraphSignatureMockBuilder commit(IGraphRepresentation graph, BigInteger rnd, final
+  // ICommitment expected) {
+  //        EasyMock.expect(mock.commit(graph, rnd)).andReturn(expected).once();
+  //        return this;
+  //    }
+  //
+  //    public IGraphSignatureMockBuilder hiddenSign(ICommitment cmt, ISignerPublicKey pk_s, final
+  // IGraphSignature expected) {
+  //        EasyMock.expect(mock.hiddenSign(cmt, pk_s)).andReturn(expected).once();
+  //        return this;
+  //    }
+  //
+  //    public IGraphSignatureMockBuilder verify(ISignerPublicKey pk_s, ICommitment cmt, BigInteger
+  // rTilde, IGraphSignature gsig, final Boolean expected) {
+  //        EasyMock.expect(mock.verify(pk_s, cmt, rTilde, gsig)).andReturn(expected).once();
+  //        return this;
+  //    }
+  //
+  //    public IGraphSignature buildAndReplay() {
+  //        EasyMock.replay(mock);
+  //        return mock;
+  //    }
 }
--- src/test/java/eu/prismacloud/primitives/grs/IGraphSignatureTest.java
+++ src/test/java/eu/prismacloud/primitives/grs/IGraphSignatureTest.java
@@ -4,31 +4,23 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-
 public class IGraphSignatureTest extends TestCase {
-    /**
-         * Create the test case
-         *
-         * @param testName name of the test case
-         */
-        public IGraphSignatureTest( String testName )
-        {
-            super( testName );
-        }
+  /**
+   * Create the test case
+   *
+   * @param testName name of the test case
+   */
+  public IGraphSignatureTest(String testName) {
+    super(testName);
+  }
 
-        /**
-         * @return the suite of tests being tested
-         */
-        public static Test suite()
-        {
-            return new TestSuite( IGraphSignatureTest.class );
-        }
+  /** @return the suite of tests being tested */
+  public static Test suite() {
+    return new TestSuite(IGraphSignatureTest.class);
+  }
 
-        /**
-         * Rigourous Test :-)
-         */
-        public void testApp()
-        {
-            assertTrue( true );
-        }
+  /** Rigourous Test :-) */
+  public void testApp() {
+    assertTrue(true);
+  }
 }
