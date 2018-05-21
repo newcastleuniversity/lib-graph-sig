53a71d9c7159f7199af8c4aaa7cf367fc9c191e9
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-05-21 01:12:41 +0100

    update code style

--- src/test/java/eu/prismacloud/primitives/grs/keys/GSSignerKeyPairTest.java
+++ src/test/java/eu/prismacloud/primitives/grs/keys/GSSignerKeyPairTest.java
@@ -1,82 +1,70 @@
 package eu.prismacloud.primitives.grs.keys;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.logging.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.logging.Logger;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-/**
- * Test Signer Key Pair
- */
+/** Test Signer Key Pair */
 class GSSignerKeyPairTest {
-    private static final Logger log = Logger.getLogger(GSSignerKeyPairTest.class.getName());
-    private GSSignerKeyPair gsk;
+  private static final Logger log = Logger.getLogger(GSSignerKeyPairTest.class.getName());
+  private GSSignerKeyPair gsk;
 
-    @BeforeEach
-    void setUp() {
-        // classUnderTest = new GSSignerKeyPair();
-        //  gsk = GSSignerKeyPair.KeyGen();
+  @BeforeEach
+  void setUp() {
+    // classUnderTest = new GSSignerKeyPair();
+    //  gsk = GSSignerKeyPair.KeyGen();
+  }
+
+  @AfterEach
+  void tearDown() {}
+
+  @Test
+  void getKeyGenSignature() {}
+
+  @Test
+  @DisplayName("Test key generation")
+  void keyGen() {
+    log.info("@Test: key generation");
+    gsk = GSSignerKeyPair.KeyGen();
+    assertNotNull(gsk);
+    assertNotNull(gsk.getPrivateKey());
+    assertNotNull(gsk.getPublicKey());
+  }
+
+  @Test
+  @DisplayName("Test key generation 10 times")
+  void keyGen10times() {
+    log.info("@Test: keyGen10times ");
+    for (int i = 0; i < 10; i++) {
+
+      gsk = GSSignerKeyPair.KeyGen();
+      assertNotNull(gsk);
     }
+  }
 
-    @AfterEach
-    void tearDown() {
+  @Test
+  void generateKeySignature() {}
 
-    }
+  @Test
+  void getPrivateKey() {
+    log.info("@Test: getPrivateKey");
+    gsk = GSSignerKeyPair.KeyGen();
+    assertNotNull(gsk.getPrivateKey());
+  }
 
-    @Test
-    void getKeyGenSignature() {
-    }
+  @Test
+  void getPublicKey() {
+    log.info("@Test: getPublickKey");
+    gsk = GSSignerKeyPair.KeyGen();
+    assertNotNull(gsk.getPublicKey());
+  }
 
-    @Test
-    @DisplayName("Test key generation")
-    void keyGen() {
-        log.info("@Test: key generation");
-        gsk = GSSignerKeyPair.KeyGen();
-        assertNotNull(gsk);
-        assertNotNull(gsk.getPrivateKey());
-        assertNotNull(gsk.getPublicKey());
-
-    }
-
-    @Test
-    @DisplayName("Test key generation 10 times")
-    void keyGen10times() {
-        log.info("@Test: keyGen10times ");
-        for (int i = 0; i < 10; i++) {
-
-            gsk = GSSignerKeyPair.KeyGen();
-            assertNotNull(gsk);
-
-        }
-    }
-
-    @Test
-    void generateKeySignature() {
-    }
-
-    @Test
-    void getPrivateKey() {
-        log.info("@Test: getPrivateKey");
-        gsk = GSSignerKeyPair.KeyGen();
-        assertNotNull(gsk.getPrivateKey());
-
-    }
-
-    @Test
-    void getPublicKey() {
-        log.info("@Test: getPublickKey");
-        gsk = GSSignerKeyPair.KeyGen();
-        assertNotNull(gsk.getPublicKey());
-    }
-
-    @Test
-    void getSignature() {
-        log.info("@Test: getSignature");
-    }
-
-
+  @Test
+  void getSignature() {
+    log.info("@Test: getSignature");
+  }
 }
--- src/test/java/eu/prismacloud/primitives/grs/keys/SignerPublicKeyTest.java
+++ src/test/java/eu/prismacloud/primitives/grs/keys/SignerPublicKeyTest.java
@@ -1,40 +1,32 @@
 package eu.prismacloud.primitives.grs.keys;
 
+import java.util.logging.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.logging.Logger;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
 class SignerPublicKeyTest {
-    private static final Logger log = Logger.getLogger(SignerPublicKeyTest.class.getName());
-       private GSSignerKeyPair gsk;
+  private static final Logger log = Logger.getLogger(SignerPublicKeyTest.class.getName());
+  private GSSignerKeyPair gsk;
 
-       @BeforeEach
-       void setUp() {
-          // classUnderTest = new GSSignerKeyPair();
-            gsk = GSSignerKeyPair.KeyGen();
-       }
-    @AfterEach
-    void tearDown() {
-    }
+  @BeforeEach
+  void setUp() {
+    // classUnderTest = new GSSignerKeyPair();
+    gsk = GSSignerKeyPair.KeyGen();
+  }
 
-    @Test
-    void getN() {
-    }
+  @AfterEach
+  void tearDown() {}
 
-    @Test
-    void getR_0() {
-    }
+  @Test
+  void getN() {}
 
-    @Test
-    void getS() {
-    }
+  @Test
+  void getR_0() {}
 
-    @Test
-    void getZ() {
-    }
+  @Test
+  void getS() {}
+
+  @Test
+  void getZ() {}
 }
