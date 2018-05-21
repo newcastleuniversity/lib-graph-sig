2d1cc2f25f6613dcd187cd6e171f34d6760a6625
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-05-21 01:12:10 +0100

    update code style

--- src/main/java/eu/prismacloud/primitives/grs/parameters/GraphEncodingParameters.java
+++ src/main/java/eu/prismacloud/primitives/grs/parameters/GraphEncodingParameters.java
@@ -1,31 +1,31 @@
 package eu.prismacloud.primitives.grs.parameters;
 
-
 public enum GraphEncodingParameters {
-    /** Maximal number of vertices to be encoded */
-    l_V(1000),
+  /** Maximal number of vertices to be encoded */
+  l_V(1000),
 
-    /** Reserved bit length for vertex encoding (bit length of
-    the largest encodable prime representative) */
-    l_prime_V(120),
+  /**
+   * Reserved bit length for vertex encoding (bit length of the largest encodable prime
+   * representative)
+   */
+  l_prime_V(120),
 
-    /** Maximal number of edges to be encoded */
-    l_E(50000),
+  /** Maximal number of edges to be encoded */
+  l_E(50000),
 
-    /** Maximal number of labels to be encoded */
-    l_L(256),
-    
-    /** Reserved bit length for label encoding */
-    l_prime_L(16);
+  /** Maximal number of labels to be encoded */
+  l_L(256),
 
+  /** Reserved bit length for label encoding */
+  l_prime_L(16);
 
-    private final int paramValue;
+  private final int paramValue;
 
-    GraphEncodingParameters(int paramValue) {
-        this.paramValue = paramValue;
-    }
+  GraphEncodingParameters(int paramValue) {
+    this.paramValue = paramValue;
+  }
 
-    public int getValue() {
-        return paramValue;
-    }
+  public int getValue() {
+    return paramValue;
+  }
 }
