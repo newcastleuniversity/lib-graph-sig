82ed159c06e01859009dac3b9ba0f49b4b8308cb
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-06-22 13:27:33 +0100

    update interface for commitments
    refactor code for commitments
    add assertions in constructor

--- src/main/java/eu/prismacloud/primitives/zkpgs/commitment/GSCommitment.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/commitment/GSCommitment.java
@@ -1,38 +1,114 @@
 package eu.prismacloud.primitives.zkpgs.commitment;
 
-import eu.prismacloud.primitives.zkpgs.keys.SignerPublicKey;
+import eu.prismacloud.primitives.zkpgs.util.Assert;
 import java.math.BigInteger;
 
-public class GSCommitment implements ICommitment {
-  private BigInteger capR;
-  private BigInteger capS;
-  private BigInteger n;
-  private int basesNumber;
+public class GSCommitment { // implements ICommitment {
 
-  private final BigInteger value;
+  private BigInteger commitmentValue;
+  private final BigInteger baseR;
+  private final BigInteger exponent;
+  private final BigInteger randomness;
+  private final BigInteger baseS;
+  private final BigInteger modN;
 
-  public GSCommitment(final BigInteger value, final SignerPublicKey spk) {
-    /* TODO create a commitment */
-    this.value = value;
+  public GSCommitment(
+      BigInteger baseR,
+      BigInteger exponent,
+      BigInteger randomness,
+      BigInteger baseS,
+      BigInteger modN) {
+
+    /** TODO add baseS group element */
+    /** TODO change name to baseS */
+    /** TODO multiple bases for commitments */
+    Assert.notNull(baseR, "base R cannot be null");
+    Assert.notNull(exponent, "exponent cannot be null");
+    Assert.notNull(randomness, "randomness cannot be null");
+    Assert.notNull(baseS, "base S cannot be null");
+    Assert.notNull(modN, "modN cannot be null");
+
+    this.baseR = baseR;
+    this.exponent = exponent;
+    this.randomness = randomness;
+    this.baseS = baseS;
+    this.modN = modN;
   }
 
-  public BigInteger getCommitment() {
-    return value;
+  public BigInteger commit() {
+
+    commitmentValue = baseR.modPow(exponent, modN).multiply(baseS.modPow(randomness, modN));
+    return commitmentValue;
   }
 
-  //    public BigInteger getCapR() {
-  //        return null;
-  //    }
-  //
-  //    public BigInteger getCapS() {
-  //        return null;
-  //    }
-  //
-  //    public BigInteger getN() {
-  //        return null;
-  //    }
-  //
-  //    public int getNumBases() {
-  //        return 0;
-  //    }
+  public BigInteger getCommitmentValue() {
+    return this.commitmentValue;
+  }
+
+  public BigInteger getBaseR() {
+    return this.baseR;
+  }
+
+  public BigInteger getExponent() {
+    return this.exponent;
+  }
+
+  public BigInteger getRandomness() {
+    return this.randomness;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || this.getClass() != o.getClass()) {
+      return false;
+    }
+
+    GSCommitment that = (GSCommitment) o;
+
+    if (!this.getCommitmentValue().equals(that.getCommitmentValue())) {
+      return false;
+    }
+    if (!this.getBaseR().equals(that.getBaseR())) {
+      return false;
+    }
+    if (!this.getExponent().equals(that.getExponent())) {
+      return false;
+    }
+    if (!this.getRandomness().equals(that.getRandomness())) {
+      return false;
+    }
+    if (!this.baseS.equals(that.baseS)) {
+      return false;
+    }
+    return this.modN.equals(that.modN);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = this.getCommitmentValue().hashCode();
+    result = 31 * result + this.getBaseR().hashCode();
+    result = 31 * result + this.getExponent().hashCode();
+    result = 31 * result + this.getRandomness().hashCode();
+    result = 31 * result + this.baseS.hashCode();
+    result = 31 * result + this.modN.hashCode();
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb =
+        new StringBuilder("eu.prismacloud.primitives.zkpgs.commitment.GSCommitment{");
+    sb.append("commitmentValue=").append(commitmentValue);
+    sb.append(", baseR=").append(baseR);
+    sb.append(", exponent=").append(exponent);
+    sb.append(", randomness=").append(randomness);
+    sb.append(", baseS=").append(baseS);
+    sb.append(", modN=").append(modN);
+    sb.append(", commit=").append(commit());
+    sb.append('}');
+    return sb.toString();
+  }
 }
--- src/main/java/eu/prismacloud/primitives/zkpgs/commitment/ICommitment.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/commitment/ICommitment.java
@@ -1,15 +1,22 @@
 package eu.prismacloud.primitives.zkpgs.commitment;
 
+import eu.prismacloud.primitives.zkpgs.BaseRepresentation;
+import eu.prismacloud.primitives.zkpgs.util.URN;
 import java.math.BigInteger;
+import java.util.Map;
 
 public interface ICommitment {
   BigInteger getCommitment();
+
+  Map<URN, BaseRepresentation> getVertices();
+
+  Map<URN, BaseRepresentation> getEdges();
 
   //    BigInteger getCapR();
   //
   //    BigInteger getCapS();
   //
-  //    BigInteger getN();
+  //    BigInteger getModN();
   //
   //    int getNumBases();
 }
