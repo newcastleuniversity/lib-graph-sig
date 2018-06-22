0d566099d7767d712953c6fd16f9c65c7176dd97
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-06-22 13:45:11 +0100

    refactor interface for signer

--- src/main/java/eu/prismacloud/primitives/zkpgs/signer/ISigner.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/signer/ISigner.java
@@ -1,30 +1,32 @@
 package eu.prismacloud.primitives.zkpgs.signer;
 
+import eu.prismacloud.primitives.zkpgs.GraphSignature;
 import eu.prismacloud.primitives.zkpgs.commitment.GSCommitment;
+import eu.prismacloud.primitives.zkpgs.graph.GSEdge;
 import eu.prismacloud.primitives.zkpgs.graph.GSGraph;
 import eu.prismacloud.primitives.zkpgs.graph.GSVertex;
 import eu.prismacloud.primitives.zkpgs.keys.ExtendedPrivateKey;
 import eu.prismacloud.primitives.zkpgs.keys.ExtendedPublicKey;
-import eu.prismacloud.primitives.zkpgs.keys.IGSKeyPair;
+import eu.prismacloud.primitives.zkpgs.keys.SignerKeyPair;
 import eu.prismacloud.primitives.zkpgs.parameters.KeyGenParameters;
 import java.math.BigInteger;
 
 public interface ISigner {
-  IGSKeyPair keyGen(KeyGenParameters gs_params);
+  SignerKeyPair keyGen(KeyGenParameters gs_params);
 
-  GSCommitment commit(GSGraph gsGraph, BigInteger rnd);
+  GSCommitment commit(GSGraph<GSVertex, GSEdge> gsGraph, BigInteger rnd);
 
-  GSGraphSignature hiddenSign(
+  GraphSignature hiddenSign(
       GSCommitment cmt,
       GSVertex signerVertex,
       GSVertex recipientVertex,
       ExtendedPublicKey extendedPublicKey,
-      GSGraph gsGraph1,
+      GSGraph<GSVertex, GSEdge> gsGraph1,
       ExtendedPrivateKey extendedPrivateKey);
 
-  void setGraph(GSGraph signerGraph);
+  void setGraph(GSGraph<GSVertex, GSEdge> signerGraph);
   /*
-   * responsible for generating an appropriate key setup, certify an encoding scheme, and to sign graphs
+   * responsible for generating an appropriate key graphEncodingSetup, certify an encoding scheme, and to sign graphs
    * In the hiddenSign() protocol the signer accepts a graph commitment from the recipient, adds an issuer-known sub-graph and
    * and completes the signature with his secret key sk_signer.
    * The signer outputs a partial graph signature, subsequently completed by the recipient.
