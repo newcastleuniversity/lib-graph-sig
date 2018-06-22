b24394e56876baf957e7ad0ada20464a75e38491
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-06-22 13:47:29 +0100

    refactor group setup prover
    add method for computing witnesses, witness randomness
    add method for computing responses
    create a list of elements for the challenge

--- src/main/java/eu/prismacloud/primitives/zkpgs/prover/GroupSetupProver.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/prover/GroupSetupProver.java
@@ -1,66 +1,125 @@
 package eu.prismacloud.primitives.zkpgs.prover;
 
+import eu.prismacloud.primitives.zkpgs.BaseRepresentation;
 import eu.prismacloud.primitives.zkpgs.graph.GSEdge;
 import eu.prismacloud.primitives.zkpgs.graph.GSVertex;
+import eu.prismacloud.primitives.zkpgs.keys.ExtendedKeyPair;
+import eu.prismacloud.primitives.zkpgs.keys.ExtendedPublicKey;
+import eu.prismacloud.primitives.zkpgs.parameters.GraphEncodingParameters;
 import eu.prismacloud.primitives.zkpgs.parameters.KeyGenParameters;
+import eu.prismacloud.primitives.zkpgs.store.ProofObject;
+import eu.prismacloud.primitives.zkpgs.store.Storable;
 import eu.prismacloud.primitives.zkpgs.util.CryptoUtilsFacade;
+import eu.prismacloud.primitives.zkpgs.util.URN;
+import eu.prismacloud.primitives.zkpgs.util.crypto.GroupElement;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Vector;
+import java.util.List;
+import java.util.Map;
 
 /** */
-public class GroupSetupProver implements IProver {
+public class GroupSetupProver implements IProver, Storable {
 
+  private ExtendedKeyPair extendedKeyPair;
+  private ExtendedPublicKey ePublicKey;
   private BigInteger r_Z;
   private BigInteger r;
   private BigInteger r_0;
-  private BigInteger tilde_r_Z;
-  private BigInteger tilde_r;
-  private BigInteger tilde_r_0;
-  private BigInteger tilde_Z;
-  private BigInteger tilde_R;
-  private BigInteger tilde_R_0;
-  private BigInteger hat_r_Z;
-  private BigInteger hat_r;
-  private BigInteger hat_r_0;
+  private BigInteger tilder_Z;
+  private BigInteger tilder;
+  private BigInteger tilder_0;
+  private BigInteger tildeZ;
+  private BigInteger tildeR;
+  private BigInteger tildeR_0;
+  private BigInteger hatr_Z;
+  private BigInteger hatr;
+  private BigInteger hatr_0;
   private Iterator<GSVertex> gsVertexIterator;
   private Iterator<GSEdge> gsEdgeIterator;
-  private Vector<GSVertex> gsVertices;
-  private Vector<GSEdge> gsEdges;
+  private List<GSVertex> gsVertices;
+  private List<GSEdge> gsEdges;
   private int bitLength;
-  private BigInteger S;
-  private BigInteger N;
-  private BigInteger Z;
+  private GroupElement baseS;
+  private BigInteger modN;
+  private GroupElement baseZ;
   private BigInteger cChallenge;
-  private Vector<BigInteger> challengeList = new Vector<BigInteger>();
+  private GroupElement baseR;
+  private GroupElement baseR_0;
+  private List<BigInteger> challengeList = new ArrayList<BigInteger>();
   private KeyGenParameters keyGenParameters;
+  private GraphEncodingParameters graphEncodingParameters;
+  private Map<String, BigInteger> vertexWitnessRandomNumbers;
+  private Map<String, BigInteger> vertexWitnessBases;
+  private Map<String, BigInteger> edgeWitnessRandomNumbers;
+  private Map<String, BigInteger> edgeWitnessBases;
+  private Map<String, BigInteger> vertexResponses;
+  private Map<String, BigInteger> edgeResponses;
+  private Map<URN, BaseRepresentation> bases;
+  private Map<String, BigInteger> edgeBases;
 
   public GroupSetupProver(
-      final BigInteger S,
-      final BigInteger N,
-      final BigInteger Z,
+      final GroupElement baseS,
+      final BigInteger modN,
+      final GroupElement baseZ,
       final KeyGenParameters keyGenParameters) {
-    this.S = S;
-    this.N = N;
-    this.Z = Z;
+    this.baseS = baseS;
+    this.modN = modN;
+    this.baseZ = baseZ;
     this.keyGenParameters = keyGenParameters;
+  }
+
+  public GroupSetupProver(
+      final ExtendedKeyPair extendedKeyPair,
+      final KeyGenParameters keyGenParameters,
+      final GraphEncodingParameters graphEncodingParameters) {
+    this.extendedKeyPair = extendedKeyPair;
+    this.keyGenParameters = keyGenParameters;
+    this.graphEncodingParameters = graphEncodingParameters;
   }
 
   @Override
   public void createWitnessRandomness() {
     bitLength = computeBitlength();
-    tilde_r_Z = CryptoUtilsFacade.computeRandomNumber(bitLength);
-    tilde_r = CryptoUtilsFacade.computeRandomNumber(bitLength);
-    tilde_r_0 = CryptoUtilsFacade.computeRandomNumber(bitLength);
-    /** TODO add randomness for vertex and edge iterators */
+    tilder_Z = CryptoUtilsFacade.computeRandomNumber(bitLength);
+    tilder = CryptoUtilsFacade.computeRandomNumber(bitLength);
+    tilder_0 = CryptoUtilsFacade.computeRandomNumber(bitLength);
+    BigInteger vWitnessRandomness;
+    BigInteger eWitnessRandomness;
+
+    for (int i = 1; i <= graphEncodingParameters.getL_V(); i++) {
+      vWitnessRandomness = CryptoUtilsFacade.computeRandomNumber(bitLength);
+      vertexWitnessRandomNumbers.put("tilder_" + i, vWitnessRandomness);
+    }
+
+    for (int j = 1; j <= graphEncodingParameters.getL_E(); j++) {
+      eWitnessRandomness = CryptoUtilsFacade.computeRandomNumber(bitLength);
+      edgeWitnessRandomNumbers.put("tilder_" + j, eWitnessRandomness);
+    }
   }
 
   @Override
   public void computeWitness() {
-    tilde_Z = S.modPow(tilde_r_Z, N);
-    tilde_R = S.modPow(tilde_r, N);
-    tilde_R_0 = S.modPow(tilde_R_0, N);
-    /** TODO compute witnesses for vertex and edge iterators */
+    tildeZ = baseS.modPow(tilder_Z, modN).getValue();
+    tildeR = baseS.modPow(tilder, modN).getValue();
+    tildeR_0 = baseS.modPow(tildeR_0, modN).getValue();
+    BigInteger vWitnessBase;
+    BigInteger eWitnessBase;
+    BigInteger vWitnessRandomNumber;
+    BigInteger eWitnessRandomNumber;
+
+    for (int i = 1; i <= graphEncodingParameters.getL_V(); i++) {
+      vWitnessRandomNumber = vertexWitnessRandomNumbers.get("tilder_" + i);
+      vWitnessBase = baseS.modPow(vWitnessRandomNumber, modN).getValue();
+      vertexWitnessRandomNumbers.put("tildeR_" + i, vWitnessBase);
+    }
+
+    for (int j = 1; j <= graphEncodingParameters.getL_E(); j++) {
+      eWitnessRandomNumber = edgeWitnessRandomNumbers.get("tilder_" + j);
+      eWitnessBase = baseS.modPow(eWitnessRandomNumber, modN).getValue();
+      vertexWitnessRandomNumbers.put("tildeR_" + j, eWitnessBase);
+    }
   }
 
   @Override
@@ -71,33 +130,108 @@ public class GroupSetupProver implements IProver {
 
   @Override
   public void computeResponses() {
+    BigInteger r_Z = extendedKeyPair.getPrivateKey().getX_rZ();
+    BigInteger r = extendedKeyPair.getPrivateKey().getX_r();
+    BigInteger r_0 = extendedKeyPair.getPrivateKey().getX_r0();
+    BigInteger witnessRandomness;
+    BigInteger vertexResponse;
+    BigInteger r_i;
+    BigInteger r_j;
+    BigInteger edgeResponse;
+    vertexResponses = new HashMap<String, BigInteger>();
+    edgeResponses = new HashMap<String, BigInteger>();
 
-    hat_r_Z = tilde_r_Z.add(cChallenge.multiply(r_Z));
-    hat_r = tilde_r.add(cChallenge.multiply(r));
-    hat_r_0 = tilde_R_0.add(cChallenge.multiply(r_0));
-    /** TODO compute responses for vertex and edge iterators */
+    hatr_Z = tilder_Z.add(cChallenge.multiply(r_Z));
+    hatr = tilder.add(cChallenge.multiply(r));
+    hatr_0 = tilder_0.add(cChallenge.multiply(r_0));
+
+    /** TODO check r_i, r_j computations */
+    for (int i = 1; i <= graphEncodingParameters.getL_V(); i++) {
+      r_i =
+          extendedKeyPair
+              .getExtendedPublicKey()
+              .getBases()
+              .get(URN.createZkpgsURN("r_" + i))
+              .getExponent();
+      witnessRandomness = vertexWitnessRandomNumbers.get("tilder_" + i);
+      vertexResponse = witnessRandomness.add(cChallenge.multiply(r_i));
+      vertexResponses.put("hatr_" + i, vertexResponse);
+    }
+
+    for (int j = 1; j <= graphEncodingParameters.getL_E(); j++) {
+      r_j =
+          extendedKeyPair
+              .getExtendedPublicKey()
+              .getBases()
+              .get(URN.createZkpgsURN("r_" + j))
+              .getExponent();
+      witnessRandomness = edgeWitnessRandomNumbers.get("tilder_" + j);
+      edgeResponse = witnessRandomness.add(cChallenge.multiply(r_j));
+      edgeResponses.put("hatr" + j, edgeResponse);
+    }
   }
 
   private int computeBitlength() {
     return keyGenParameters.getL_n() + keyGenParameters.getL_statzk() + keyGenParameters.getL_H();
   }
 
-  private Vector<BigInteger> populateChallengeList() {
+  private List<BigInteger> populateChallengeList() {
     /** TODO add context to list of elements in challenge */
-    challengeList.add(N);
-    challengeList.add(S);
-    challengeList.add(Z);
-    // challengeList.add(R);
-    // challengeList.add(R_0);
-    /** TODO iterate over bases for vertices and edges */
-    // challengeList.add(R_i);
-    // challengeList.add(R_j);
-    challengeList.add(tilde_Z);
-    challengeList.add(tilde_R);
-    challengeList.add(tilde_R_0);
-    /** TODO iterate over bases for vertices and edges */
-    // challengeList.add(tilde_R_i);
-    // challengeList.add(tilde_R_j);
+    baseR = extendedKeyPair.getPublicKey().getBaseR();
+    baseR_0 = extendedKeyPair.getPublicKey().getBaseR_0();
+    bases = extendedKeyPair.getExtendedPublicKey().getBases();
+
+    challengeList.add(modN);
+    challengeList.add(baseS.getValue());
+    challengeList.add(baseZ.getValue());
+    challengeList.add(baseR.getValue());
+    challengeList.add(baseR_0.getValue());
+
+    for (BaseRepresentation baseRepresentation : bases.values()) {
+      challengeList.add(baseRepresentation.getBase().getValue());
+    }
+
+    challengeList.add(tildeZ);
+    challengeList.add(tildeR);
+    challengeList.add(tildeR_0);
+
+    /** TODO use URNs for keys in witnesses bases */
+    for (int i = 1; i <= vertexWitnessBases.size(); i++) {
+      challengeList.add(vertexWitnessBases.get("tildeR_" + i));
+    }
+
+    for (int j = 1; j <= edgeWitnessBases.size(); j++) {
+      challengeList.add(edgeWitnessBases.get("tildeR_" + j));
+    }
     return challengeList;
+  }
+
+  public ProofSignature outputProofSignature() {
+
+    Map<URN, Object> proofSignatureElements = new HashMap<>();
+
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.modN"), this.modN);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.baseS"), this.baseS);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.baseZ"), this.baseZ);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.baseR"), this.baseR);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.baseR_0"), this.baseR_0);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.R_i"), this.bases);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.R_i_j"), this.edgeBases);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.hatr_Z"), this.hatr_Z);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.hatr"), this.hatr);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.hatr_0"), this.hatr_0);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.hatr_i"), this.vertexResponses);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.hatr_i_j"), this.edgeResponses);
+    proofSignatureElements.put(URN.createZkpgsURN("proofsignature.P.c"), cChallenge);
+
+    return new ProofSignature(proofSignatureElements);
+  }
+
+  @Override
+  public void store(URN urn, ProofObject proofObject) {}
+
+  @Override
+  public ProofObject retrieve(URN urn) {
+    return null;
   }
 }
