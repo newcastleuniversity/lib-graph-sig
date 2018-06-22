f72ef850904fe06c83d93bcb845b582010f4ac86
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-06-22 12:53:51 +0100

    update urn with a helper method for creating URNs

--- src/main/java/eu/prismacloud/primitives/zkpgs/util/URN.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/util/URN.java
@@ -11,6 +11,7 @@ import eu.prismacloud.primitives.zkpgs.util.NamespaceComponent.Type;
  */
 public final class URN {
 
+  private static final String zkpgsNameSpaceIdentifier = "zkpgs";
   private static final String URN_SCHEME = "urn";
   private static final String COLON = ":";
   private final NamespaceComponent namespaceIdentifier;
@@ -24,13 +25,21 @@ public final class URN {
   }
 
   /**
+   * Gets zkpgs name space identifier.
+   *
+   * @return the zkpgs name space identifier
+   */
+  public static String getZkpgsNameSpaceIdentifier() {
+    return zkpgsNameSpaceIdentifier;
+  }
+  /**
    * Create urn from namespaceIdentifier and namespaceSpecific from NamespaceComponent objects.
    *
    * @param namespaceIdentifier the namespace identifier
    * @param namespaceSpecific the namespace specific
+   * @return the urn
    * @pre namespaceIdentifier != null && namespaceSpecific != null
    * @post
-   * @return the urn
    */
   public static URN createURN(
       final NamespaceComponent namespaceIdentifier, final NamespaceComponent namespaceSpecific) {
@@ -46,10 +55,10 @@ public final class URN {
    *
    * @param namespaceIdentifier the namespace identifier
    * @param namespaceSpecific the namespace specific
+   * @return the urn
    * @pre namespaceIdentifier != null && namespaceIdentifier != "" && namespaceSpecific != null &&
    *     namespaceSpecific != ""
    * @post
-   * @return the urn
    */
   public static URN createURN(final String namespaceIdentifier, final String namespaceSpecific) {
 
@@ -63,5 +72,59 @@ public final class URN {
         NamespaceComponent.fromString(namespaceSpecific, Type.SPECIFIC_STRING);
 
     return new URN(nic, nssc);
+  }
+
+  /**
+   * Helper method to create a zkpgs based urn.
+   *
+   * @param namespaceSpecific the namespace specific
+   * @return the urn
+   */
+  public static URN createZkpgsURN(final String namespaceSpecific) {
+    Assert.notNull(namespaceSpecific, "Namespace Specific String is required for URN");
+    Assert.notEmpty(namespaceSpecific, "Namespace Specific String must not be empty in a urn");
+
+    NamespaceComponent nic =
+        NamespaceComponent.fromString(URN.getZkpgsNameSpaceIdentifier(), Type.IDENTIFIER);
+    NamespaceComponent nssc =
+        NamespaceComponent.fromString(namespaceSpecific, Type.SPECIFIC_STRING);
+
+    return new URN(nic, nssc);
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("URN{");
+    sb.append("zkpgsNameSpaceIdentifier='").append(zkpgsNameSpaceIdentifier).append('\'');
+    sb.append(", URN_SCHEME='").append(URN_SCHEME).append('\'');
+    sb.append(", COLON='").append(COLON).append('\'');
+    sb.append(", namespaceIdentifier=").append(namespaceIdentifier);
+    sb.append(", namespaceSpecific=").append(namespaceSpecific);
+    sb.append('}');
+    return sb.toString();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || this.getClass() != o.getClass()) {
+      return false;
+    }
+
+    URN urn = (URN) o;
+
+    if (!this.namespaceIdentifier.equals(urn.namespaceIdentifier)) {
+      return false;
+    }
+    return this.namespaceSpecific.equals(urn.namespaceSpecific);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = this.namespaceIdentifier.hashCode();
+    result = 31 * result + this.namespaceSpecific.hashCode();
+    return result;
   }
 }
