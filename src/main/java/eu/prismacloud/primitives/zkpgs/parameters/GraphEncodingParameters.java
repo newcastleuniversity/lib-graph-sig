383950af9dbaca739f307cd96bea1e5746db5d40
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-06-22 13:36:40 +0100

    refactor graph encoding parameters

--- src/main/java/eu/prismacloud/primitives/zkpgs/parameters/GraphEncodingParameters.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/parameters/GraphEncodingParameters.java
@@ -5,48 +5,46 @@ import eu.prismacloud.primitives.zkpgs.util.Assert;
 /** The type Graph encoding parameters. */
 public class GraphEncodingParameters {
   /** Maximal number of vertices to be encoded */
-  private final int l_V;//(1000),
+  private final int l_V;
 
   /**
    * Reserved bit length for vertex encoding (bit length of the largest encodeable prime
    * representative)
    */
-  private final int l_prime_V;//(120),
+  private final int lPrime_V;
 
   /** Maximal number of edges to be encoded */
-  private final int l_E;//(50000),
+  private final int l_E;
 
   /** Maximal number of labels to be encoded */
-  private final int l_L;//(256),
+  private final int l_L;
 
   /** Reserved bit length for label encoding */
-  private final int l_prime_L;//(16);
+  private final int lPrime_L;
 
   /**
    * Instantiates a new Graph encoding parameters.
    *
    * @param l_V the maximal number of vertices to be encoded
-   * @param l_prime_V the bit length for vertex encoding
+   * @param lPrime_V the bit length for vertex encoding
    * @param l_E the maximal number of edges to be encoded
    * @param l_L the maximal number of labels to be encoded
-   * @param l_prime_L the reserved bit length for label encoding
-   * 
-   * @pre l_V != null && l_prime_V != null && l_E != null && l_L != null && l_prime_L != null
+   * @param lPrime_L the reserved bit length for label encoding
+   * @pre l_V != null && lPrime_V != null && l_E != null && l_L != null && lPrime_L != null
    * @post
    */
-  public GraphEncodingParameters(int l_V, int l_prime_V, int l_E, int l_L, int l_prime_L) {
+  public GraphEncodingParameters(int l_V, int lPrime_V, int l_E, int l_L, int lPrime_L) {
     Assert.notNull(l_V, "l_V parameter must not be null");
-    Assert.notNull(l_prime_V, "l_prime_V parameter must not be null");
+    Assert.notNull(lPrime_V, "lPrime_V parameter must not be null");
     Assert.notNull(l_E, "l_E parameter must not be null");
     Assert.notNull(l_L, "l_L parameter must not be null");
-    Assert.notNull(l_prime_L, "l_prime_L parameter must not be null");
-
+    Assert.notNull(lPrime_L, "lPrime_L parameter must not be null");
 
     this.l_V = l_V;
-    this.l_prime_V = l_prime_V;
+    this.lPrime_V = lPrime_V;
     this.l_E = l_E;
     this.l_L = l_L;
-    this.l_prime_L = l_prime_L;
+    this.lPrime_L = lPrime_L;
   }
 
   /**
@@ -63,8 +61,8 @@ public class GraphEncodingParameters {
    *
    * @return the l prime v
    */
-  public int getL_prime_V() {
-    return l_prime_V;
+  public int getlPrime_V() {
+    return lPrime_V;
   }
 
   /**
@@ -90,7 +88,7 @@ public class GraphEncodingParameters {
    *
    * @return the l prime l
    */
-  public int getL_prime_L() {
-    return l_prime_L;
+  public int getlPrime_L() {
+    return lPrime_L;
   }
 }
