7a1ea87f7cbd6b5aac23b362a2ebc499758f98c1
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-06-22 13:14:56 +0100

    refactor code for groupelements
    add assertions in constructor

--- src/main/java/eu/prismacloud/primitives/zkpgs/util/crypto/QRGroupPQ.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/util/crypto/QRGroupPQ.java
@@ -1,11 +1,13 @@
 package eu.prismacloud.primitives.zkpgs.util.crypto;
 
+import eu.prismacloud.primitives.zkpgs.util.Assert;
 import eu.prismacloud.primitives.zkpgs.util.CryptoUtilsFacade;
 import eu.prismacloud.primitives.zkpgs.util.NumberConstants;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.List;
 
-/** Quadratic Residues Group when the modulus factorization is known */
+/** Quadratic Residues Group when the modulus factorization is known. */
 public final class QRGroupPQ extends Group {
 
   private final BigInteger modulus;
@@ -15,26 +17,31 @@ public final class QRGroupPQ extends Group {
   private final BigInteger oneP;
   private final BigInteger oneQ;
   private GroupElement generator;
-  private ArrayList<GroupElement> groupElements = new ArrayList<>();
+  private final List<GroupElement> groupElements = new ArrayList<>();
 
   /**
    * Instantiates a new QR group where we know the modulus factorization.
    *
    * @param pPrime the p prime
    * @param qPrime the q prime
+   * @pre pPrime != null && qPrime != null
+   * @post
    */
   public QRGroupPQ(final BigInteger pPrime, final BigInteger qPrime) {
+
+    Assert.notNull(pPrime, "pPrime must not be null");
+    Assert.notNull(qPrime, "qPrime must not be null");
 
     this.modulus = pPrime.multiply(qPrime);
     this.pPrime = pPrime;
     this.qPrime = qPrime;
-    this.order = getOrder();
-    computeEEA(pPrime, qPrime);
+    this.order = this.getOrder();
+    QRGroupPQ.computeEEA(pPrime, qPrime);
     this.oneP = CRT.compute1p(EEAlgorithm.getT(), pPrime, qPrime);
     this.oneQ = CRT.compute1q(EEAlgorithm.getS(), pPrime, qPrime);
   }
 
-  private static void computeEEA(BigInteger p, BigInteger q) {
+  private static void computeEEA(final BigInteger p, final BigInteger q) {
     EEAlgorithm.computeEEAlgorithm(p, q);
   }
 
@@ -55,10 +62,11 @@ public final class QRGroupPQ extends Group {
   }
 
   /**
-   * Create generator group element for QRN. when the modulus factorization is known.
+   * Create generator group element for QRN when the modulus factorization is known.
    *
    * @return the group element
    */
+  @Override
   public GroupElement createGenerator() {
     return this.generator =
         new QRElementPQ(this, CryptoUtilsFacade.computeQRNGenerator(this.modulus), pPrime, qPrime);
@@ -66,19 +74,18 @@ public final class QRGroupPQ extends Group {
 
   @Override
   public GroupElement createElement() {
-
     return new QRElementPQ(this, CryptoUtilsFacade.computeQRNElement(this.modulus), pPrime, qPrime);
   }
 
   @Override
-  public GroupElement createElement(GroupElement s) {
+  public GroupElement createElement(final GroupElement s) {
     QRElementPQ qrElementPQ;
     BigInteger upperBound = this.pPrime.multiply(this.qPrime).subtract(BigInteger.ONE);
 
     do {
       BigInteger exponent =
           CryptoUtilsFacade.computeRandomNumber(NumberConstants.TWO.getValue(), upperBound);
-      qrElementPQ = new QRElementPQ(this, s.modPow(exponent, this.modulus));
+      qrElementPQ = new QRElementPQ(this, s.modPow(exponent, this.modulus).getValue());
 
     } while (!isElement(qrElementPQ.getValue()));
 
@@ -96,7 +103,7 @@ public final class QRGroupPQ extends Group {
    * @return true if alpha in QRN, false if alpha not in QRN Dependencies: jacobiSymbol()
    */
   @Override
-  public boolean isElement(BigInteger alpha) {
+  public boolean isElement(final BigInteger alpha) {
     // TODO check if computations are correct
     return CryptoUtilsFacade.isElementOfQR(alpha, pPrime)
         && CryptoUtilsFacade.isElementOfQR(alpha, qPrime);
@@ -110,7 +117,8 @@ public final class QRGroupPQ extends Group {
    * @param qPrime the q prime
    * @return true if S is a generator of QRN or false if it is not
    */
-  public boolean verifySGenerator(BigInteger S, BigInteger pPrime, BigInteger qPrime) {
+  public boolean verifySGenerator(
+      final BigInteger S, final BigInteger pPrime, final BigInteger qPrime) {
     if (!S.equals(BigInteger.ONE.mod(modulus))) {
       if (!S.modPow(pPrime, modulus).equals(BigInteger.ONE.mod(modulus)))
         return !S.modPow(qPrime, modulus).equals(BigInteger.ONE.mod(modulus));
@@ -124,17 +132,27 @@ public final class QRGroupPQ extends Group {
    * implementation).
    *
    * @param S the generator
-   * @param N the modulus
+   * @param modN the modulus
    * @return true if S is a generator of QRN or false if it is not
    */
-  public boolean verifySGenerator(BigInteger S, BigInteger N) {
-    return S.subtract(BigInteger.ONE).gcd(N).equals(BigInteger.ONE);
+  public boolean verifySGenerator(final BigInteger S, final BigInteger modN) {
+    return S.subtract(BigInteger.ONE).gcd(modN).equals(BigInteger.ONE);
   }
 
+  /**
+   * Gets one p.
+   *
+   * @return the one p
+   */
   public BigInteger getOneP() {
     return oneP;
   }
 
+  /**
+   * Gets one q.
+   *
+   * @return the one q
+   */
   public BigInteger getOneQ() {
     return oneQ;
   }
