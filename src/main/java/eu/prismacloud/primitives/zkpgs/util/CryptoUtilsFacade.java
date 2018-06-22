0110e0570dea75c479292dd76b3bbee37baaef14
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-06-22 13:07:13 +0100

    add enum for cryptoutils
    add method for multibaseex
    refactore code with enum

--- src/main/java/eu/prismacloud/primitives/zkpgs/util/CryptoUtilsFacade.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/util/CryptoUtilsFacade.java
@@ -4,55 +4,77 @@ import eu.prismacloud.primitives.zkpgs.util.crypto.CommitmentGroup;
 import eu.prismacloud.primitives.zkpgs.util.crypto.SafePrime;
 import eu.prismacloud.primitives.zkpgs.util.crypto.SpecialRSAMod;
 import java.math.BigInteger;
-import java.util.Vector;
+import java.util.List;
 
 /**
  * Wrapper Class for low-level number theoretic computations. Choose which implementation to use.
  */
 public class CryptoUtilsFacade {
+  private static final String GS = "GS";
+  private static final String IDEMIX = "IDEMIX";
+
+  private enum cryptoUtil {
+    GS,
+    IDEMIX
+  };
 
   public CryptoUtilsFacade() {}
 
+  public static BigInteger computeMultiBaseEx(
+      List<BigInteger> bases, List<BigInteger> exponents, BigInteger N) {
+    return CryptoUtilsFactory.getInstance(GS).multiBaseExp(bases, exponents, N);
+  }
+
+  public static BigInteger computePrimeWithLength(int minBitLength, int maxBitLength) {
+    return CryptoUtilsFactory.getInstance(GS).generatePrimeWithLength(minBitLength, maxBitLength);
+  }
+
   public static BigInteger computeRandomNumber(int bitLength) {
-    return CryptoUtilsFactory.getInstance("GS").createRandomNumber(bitLength);
+    return CryptoUtilsFactory.getInstance(GS).createRandomNumber(bitLength);
   }
 
-  public static BigInteger computeHash(Vector<BigInteger> list, int hashLength) {
+  public static BigInteger computeHash(List<BigInteger> list, int hashLength) {
 
-    return CryptoUtilsFactory.getInstance("GS").calculateHash(list, hashLength);
-    
+    return CryptoUtilsFactory.getInstance(GS).calculateHash(list, hashLength);
   }
 
+  public static BigInteger computeA() {
+    return CryptoUtilsFactory.getInstance(GS).computeA();
+  }
 
   public SafePrime computeRandomSafePrime() {
-    return CryptoUtilsFactory.getInstance("GS").generateRandomSafePrime();
+    return CryptoUtilsFactory.getInstance(GS).generateRandomSafePrime();
   }
 
   public static SpecialRSAMod computeSpecialRSAModulus() {
-    return CryptoUtilsFactory.getInstance("GS").generateSpecialRSAModulus();
+    return CryptoUtilsFactory.getInstance(GS).generateSpecialRSAModulus();
   }
 
   public static BigInteger computeQRNGenerator(BigInteger n) {
-    return CryptoUtilsFactory.getInstance("GS").createQRNGenerator(n);
+    return CryptoUtilsFactory.getInstance(GS).createQRNGenerator(n);
   }
 
   public static BigInteger computeQRNElement(BigInteger n) {
-    return CryptoUtilsFactory.getInstance("GS").createQRNElement(n);
+    return CryptoUtilsFactory.getInstance(GS).createQRNElement(n);
   }
 
   public static BigInteger computeRandomNumber(BigInteger lowerBound, BigInteger upperBound) {
-    return CryptoUtilsFactory.getInstance("GS").createRandomNumber(lowerBound, upperBound);
+    return CryptoUtilsFactory.getInstance(GS).createRandomNumber(lowerBound, upperBound);
   }
 
   public static CommitmentGroup commitmentGroupSetup() {
-    return CryptoUtilsFactory.getInstance("IDEMIX").generateCommitmentGroup();
+    return CryptoUtilsFactory.getInstance(IDEMIX).generateCommitmentGroup();
   }
 
   public static BigInteger commitmentGroupGenerator(BigInteger rho, BigInteger gamma) {
-    return CryptoUtilsFactory.getInstance("GS").createCommitmentGroupGenerator(rho, gamma);
+    return CryptoUtilsFactory.getInstance(GS).createCommitmentGroupGenerator(rho, gamma);
   }
 
   public static Boolean isElementOfQR(BigInteger value, BigInteger modulus) {
-    return CryptoUtilsFactory.getInstance("GS").elementOfQRN(value, modulus);
+    return CryptoUtilsFactory.getInstance(GS).elementOfQRN(value, modulus);
+  }
+
+  public static BigInteger generateRandomPrime(int bitLength) {
+    return CryptoUtilsFactory.getInstance(GS).generateRandomPrime(bitLength);
   }
 }
