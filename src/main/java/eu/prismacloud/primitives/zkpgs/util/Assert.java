f1a2e97eebd9b554835df2de5fd28a4ae8565b16
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-06-22 12:56:49 +0100

    add method for checking bitlength of a biginteger
    add method for checking size of two int numbers

--- src/main/java/eu/prismacloud/primitives/zkpgs/util/Assert.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/util/Assert.java
@@ -1,5 +1,7 @@
 package eu.prismacloud.primitives.zkpgs.util;
 
+import java.math.BigInteger;
+
 /** Common precondition checks for methods and constructor */
 public final class Assert {
 
@@ -8,6 +10,7 @@ public final class Assert {
   /**
    * Evaluates object parameter if it is null or not.
    *
+   * @param <T> the type parameter
    * @param object the object
    * @param errorMsg specifies the error message for the exception
    * @return the t
@@ -21,10 +24,43 @@ public final class Assert {
     return object;
   }
 
+  /**
+   * Not empty string.
+   *
+   * @param text the text
+   * @param errorMsg the error msg
+   * @return the string
+   */
   public static String notEmpty(String text, String errorMsg) {
     if (text.isEmpty()) {
       throw new IllegalArgumentException(errorMsg);
     }
     return text;
+  }
+
+  /**
+   * Check length of BigInteger number.
+   *
+   * @param number the number to check length
+   * @param length the length
+   * @param errorMsg the error msg
+   */
+  public static void checkBitLength(BigInteger number, int length, String errorMsg) {
+    if (number.bitLength() != length) {
+      throw new IllegalArgumentException(errorMsg);
+    }
+  }
+
+  /**
+   * Check size of two numbers.
+   *
+   * @param sizeA the size a
+   * @param sizeB the size b
+   * @param errorMsg the error msg
+   */
+  public static void checkSize(int sizeA, int sizeB, String errorMsg) {
+    if (sizeA != sizeB) {
+      throw new IllegalArgumentException(errorMsg);
+    }
   }
 }
