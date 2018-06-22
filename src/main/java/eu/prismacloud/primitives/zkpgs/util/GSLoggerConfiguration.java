1d16f16d20b4910be55fc1e468b96e39da1d7814
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-06-22 12:57:24 +0100

    update gslog with a configuration file

--- src/main/java/eu/prismacloud/primitives/zkpgs/util/GSLoggerConfiguration.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/util/GSLoggerConfiguration.java
@@ -5,21 +5,26 @@ import java.io.InputStream;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
-/** Configure Java logger */
+/** Configure utility logger class for the graph signature library. */
 public class GSLoggerConfiguration {
 
-  private static Logger gslog = null;
+  private static Logger gslog;
+
+  private GSLoggerConfiguration() {}
+
+  private static final String LOGGING_FILE = "logging.properties";
 
   static {
     InputStream stream =
-        GSLoggerConfiguration.class.getClassLoader().getResourceAsStream("logging.properties");
+        GSLoggerConfiguration.class.getClassLoader().getResourceAsStream(LOGGING_FILE);
+
     try {
 
       LogManager.getLogManager().readConfiguration(stream);
       gslog = Logger.getLogger(GSLoggerConfiguration.class.getName());
 
     } catch (IOException e) {
-      System.err.println("logging.properties files cannot be opened" + e.getMessage());
+      System.err.println("logging.properties files cannot be opened " + e.getMessage());
     }
   }
 
