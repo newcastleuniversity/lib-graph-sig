29195b2609f6b746df91a64db7a46d57550b501f
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-06-22 12:52:37 +0100

    add baserepresentation class to hold the bases and related elements

---
+++ src/main/java/eu/prismacloud/primitives/zkpgs/BaseRepresentation.java
@@ -0,0 +1,65 @@
+package eu.prismacloud.primitives.zkpgs;
+
+import eu.prismacloud.primitives.zkpgs.util.crypto.GroupElement;
+import java.math.BigInteger;
+
+/** */
+public class BaseRepresentation {
+
+  public enum BASE {
+    VERTEX,
+    EDGE
+  };
+
+  private final int baseIndex;
+  private final BASE baseType;
+  private final GroupElement base;
+  private BigInteger exponent;
+
+  public BaseRepresentation(GroupElement base, BigInteger exponent, int baseIndex, BASE baseType) {
+
+    this.base = base;
+    this.exponent = exponent;
+    this.baseIndex = baseIndex;
+    this.baseType = baseType;
+  }
+
+  public BaseRepresentation(GroupElement base, int baseIndex, BASE baseType) {
+
+    this.base = base;
+    this.baseIndex = baseIndex;
+    this.baseType = baseType;
+  }
+
+  public int getBaseIndex() {
+    return this.baseIndex;
+  }
+
+  public void setExponent(BigInteger exponentEncoding) {
+    this.exponent = exponentEncoding;
+  }
+
+  public GroupElement getBase() {
+    return this.base;
+  }
+
+  public BigInteger getExponent() {
+    return this.exponent;
+  }
+
+  public BASE getBaseType() {
+    return this.baseType;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder(
+        "eu.prismacloud.primitives.zkpgs.BaseRepresentation{");
+    sb.append("baseIndex=").append(baseIndex);
+    sb.append(", baseType=").append(baseType);
+    sb.append(", base=").append(base);
+    sb.append(", exponent=").append(exponent);
+    sb.append('}');
+    return sb.toString();
+  }
+}
