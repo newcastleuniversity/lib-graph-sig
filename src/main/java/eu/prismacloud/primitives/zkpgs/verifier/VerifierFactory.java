ecdfee3435ffddd69d258a0f520f764a6e8127aa
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-06-27 23:29:45 +0100

    add verifier factory

---
+++ src/main/java/eu/prismacloud/primitives/zkpgs/verifier/VerifierFactory.java
@@ -0,0 +1,28 @@
+package eu.prismacloud.primitives.zkpgs.verifier;
+
+/** */
+public class VerifierFactory {
+  public enum VerifierType {
+    CommitmentVerifier,
+    GroupSetupVerifier,
+    CorrectnessVerifier,
+    PossessionVerifier,
+    PairWiseDifferenceVerifier
+  }
+
+  public static IVerifier newVerifier(VerifierType type) {
+    if (type == VerifierType.GroupSetupVerifier) {
+      return new GroupSetupVerifier();
+    } else if (type == VerifierType.CommitmentVerifier) {
+      return new CommitmentVerifier();
+    } else if (type == VerifierType.CorrectnessVerifier) {
+      return new CorrectnessVerifier();
+    } else if (type == VerifierType.PossessionVerifier) {
+      return new GSPossessionVerifier();
+    } else if (type == VerifierType.PairWiseDifferenceVerifier) {
+      return new PairWiseDifferenceVerifier();
+    } else {
+      return null;
+    }
+  }
+}
