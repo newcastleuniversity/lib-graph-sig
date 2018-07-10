b2082c908325a46b84c0fa1a6469d21a62851239
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-07-10 18:07:22 +0100

    fix package for group setup test

--- src/test/java/eu/prismacloud/primitives/zkpgs/GroupSetupIT.java
+++ src/test/java/eu/prismacloud/primitives/zkpgs/GroupSetupIT.java
@@ -1,4 +1,4 @@
-package eu.prismacloud.primitives.zkpgs.orchestrator;
+package eu.prismacloud.primitives.zkpgs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
