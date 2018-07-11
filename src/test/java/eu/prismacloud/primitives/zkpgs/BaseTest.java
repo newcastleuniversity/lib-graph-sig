db5edff88d35ada50ba620d37b0da1e36e470dff
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-07-11 14:10:36 +0100

    added BaseTest class for persisted SignerKeyPair for test cases

---
+++ src/test/java/eu/prismacloud/primitives/zkpgs/BaseTest.java
@@ -0,0 +1,61 @@
+package eu.prismacloud.primitives.zkpgs;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import eu.prismacloud.primitives.zkpgs.keys.SignerKeyPair;
+import eu.prismacloud.primitives.zkpgs.keys.SignerPublicKey;
+import eu.prismacloud.primitives.zkpgs.parameters.GraphEncodingParameters;
+import eu.prismacloud.primitives.zkpgs.parameters.JSONParameters;
+import eu.prismacloud.primitives.zkpgs.parameters.KeyGenParameters;
+import eu.prismacloud.primitives.zkpgs.util.FilePersistenceUtil;
+import java.io.IOException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+/** Base test class for using persisted SignerKeyPair */
+public class BaseTest {
+  public static final String MODULUS_BIT_LENGTH = "2048";
+  private KeyGenParameters keyGenParameters;
+  private SignerKeyPair gsk;
+
+  public KeyGenParameters getKeyGenParameters() {
+    return this.keyGenParameters;
+  }
+
+  public GraphEncodingParameters getGraphEncodingParameters() {
+    return this.graphEncodingParameters;
+  }
+
+  private GraphEncodingParameters graphEncodingParameters;
+
+  @BeforeAll
+  public void setup() {
+    JSONParameters parameters = new JSONParameters();
+    keyGenParameters = parameters.getKeyGenParameters();
+    graphEncodingParameters = parameters.getGraphEncodingParameters();
+  }
+
+  @ParameterizedTest(name = "{index} => bitLength=''{0}''")
+  @ValueSource(strings = {"2048"})
+  public void shouldCreateASignerKeyPair(String bitLength)
+      throws IOException, ClassNotFoundException {
+
+    if (bitLength.equals("2048")) {
+      FilePersistenceUtil persistenceUtil = new FilePersistenceUtil();
+      gsk = (SignerKeyPair) persistenceUtil.read("SignerKeyPair-" + bitLength + ".ser");
+    } else {
+      gsk.keyGen(keyGenParameters);
+    }
+
+    assertNotNull(gsk);
+    assertNotNull(gsk.getPrivateKey());
+    assertNotNull(gsk.getPublicKey());
+  }
+
+  public SignerKeyPair getSignerKeyPair() {
+    return gsk;
+  }
+}
