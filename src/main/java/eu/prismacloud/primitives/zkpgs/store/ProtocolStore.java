ebf0e74763b150983bc28650f7100905e01a8671
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-07-13 01:21:04 +0100

    clean up code

--- src/main/java/eu/prismacloud/primitives/zkpgs/store/ProtocolStore.java
+++
@@ -1,3 +0,0 @@
-package eu.prismacloud.primitives.zkpgs.store;
-
-public class ProtocolStore {}
--- src/main/java/eu/prismacloud/primitives/zkpgs/store/Storable.java
+++
@@ -1,9 +0,0 @@
-package eu.prismacloud.primitives.zkpgs.store;
-
-import eu.prismacloud.primitives.zkpgs.util.URN;
-
-public interface Storable {
-  void store(URN urn, ProofObject proofObject);
-
-  ProofObject retrieve(URN urn);
-}
