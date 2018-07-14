6044e77ebcf0ea436514673d2abc82ada4b28b9f
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-07-14 01:17:20 +0100

    add support for multiple labels in edges
    add tests for GSEdge

--- src/main/java/eu/prismacloud/primitives/zkpgs/graph/GSEdge.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/graph/GSEdge.java
@@ -1,13 +1,16 @@
 package eu.prismacloud.primitives.zkpgs.graph;
 
+import java.math.BigInteger;
+import java.util.List;
 import org.jgrapht.graph.DefaultEdge;
 
-/** Representing a edge message in a graph signature. */
+/** Representing a edge in a graph. */
 public class GSEdge extends DefaultEdge {
 
   private GSVertex e_i;
   private GSVertex e_j;
-  private String labelRepresentative;
+  private List<BigInteger> labelRepresentatives;
+  private List<String> labels;
 
   public GSEdge(GSVertex e_i, GSVertex e_j) {
     this.e_i = e_i;
@@ -22,49 +25,58 @@ public class GSEdge extends DefaultEdge {
     return e_j;
   }
 
-  public String getLabelRepresentative() {
-    return labelRepresentative;
+  public List<BigInteger> getLabelRepresentatives() {
+    return labelRepresentatives;
   }
 
-  public void setLabelRepresentative(String labelRepresentative) {
-    this.labelRepresentative = labelRepresentative;
+  public void setLabelRepresentatives(List<BigInteger> labelRepresentatives) {
+    this.labelRepresentatives = labelRepresentatives;
   }
+
+  public List<String> getLabels() {
+    return labels;
+  }
+
+  public void setLabels(List<String> labels) {
+    this.labels = labels;
+  }
+
+  //  @Override
+  //  public boolean equals(Object o) {
+  //    if (this == o) {
+  //      return true;
+  //    }
+  //    if (o == null || this.getClass() != o.getClass()) {
+  //      return false;
+  //    }
+  //
+  //    GSEdge gsEdge = (GSEdge) o;
+  //
+  //    if (!this.getE_i().equals(gsEdge.getE_i())) {
+  //      return false;
+  //    }
+  //    if (!this.getE_j().equals(gsEdge.getE_j())) {
+  //      return false;
+  //    }
+  //    return this.getLabelRepresentatives().equals(gsEdge.getLabelRepresentatives());
+  //  }
+  //
+  //  @Override
+  //  public int hashCode() {
+  //    int result = this.getE_i().hashCode();
+  //    result = 31 * result + this.getE_j().hashCode();
+  //    result = 31 * result + this.getLabelRepresentatives().hashCode();
+  //    return result;
+  //  }
 
   @Override
   public String toString() {
     final StringBuilder sb = new StringBuilder("eu.prismacloud.primitives.zkpgs.graph.GSEdge{");
     sb.append("e_i=").append(e_i);
     sb.append(", e_j=").append(e_j);
-    sb.append(", labelRepresentative='").append(labelRepresentative).append('\'');
+    sb.append(", labelRepresentatives=").append(labelRepresentatives);
+    sb.append(", labels=").append(labels);
     sb.append('}');
     return sb.toString();
   }
-
-//  @Override
-//  public boolean equals(Object o) {
-//    if (this == o) {
-//      return true;
-//    }
-//    if (o == null || this.getClass() != o.getClass()) {
-//      return false;
-//    }
-//
-//    GSEdge gsEdge = (GSEdge) o;
-//
-//    if (!this.getE_i().equals(gsEdge.getE_i())) {
-//      return false;
-//    }
-//    if (!this.getE_j().equals(gsEdge.getE_j())) {
-//      return false;
-//    }
-//    return this.getLabelRepresentative().equals(gsEdge.getLabelRepresentative());
-//  }
-//
-//  @Override
-//  public int hashCode() {
-//    int result = this.getE_i().hashCode();
-//    result = 31 * result + this.getE_j().hashCode();
-//    result = 31 * result + this.getLabelRepresentative().hashCode();
-//    return result;
-//  }
 }
---
+++ src/test/java/eu/prismacloud/primitives/zkpgs/graph/GSEdgeTest.java
@@ -0,0 +1,69 @@
+package eu.prismacloud.primitives.zkpgs.graph;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GSEdgeTest {
+
+  private GSVertex e_i;
+  private GSVertex e_j;
+  private GSEdge gsEdge;
+
+  @BeforeEach
+  void setUp() {
+    e_i = new GSVertex("1", new ArrayList<>());
+    e_i.setVertexPrimeRepresentative(BigInteger.valueOf(6701));
+    e_j = new GSVertex("2", new ArrayList<>());
+    e_j.setVertexPrimeRepresentative(BigInteger.valueOf(7703));
+  }
+
+  @Test
+  void getE_i() {
+    gsEdge = new GSEdge(e_i, e_j);
+    assertNotNull(gsEdge.getE_i());
+    assertEquals(BigInteger.valueOf(6701), gsEdge.getE_i().getVertexPrimeRepresentative());
+  }
+
+  @Test
+  void getE_j() {
+    gsEdge = new GSEdge(e_i, e_j);
+    assertNotNull(gsEdge.getE_j());
+    assertEquals(BigInteger.valueOf(7703), gsEdge.getE_j().getVertexPrimeRepresentative());
+  }
+
+  @Test
+  void getLabelRepresentatives() {
+    gsEdge = new GSEdge(e_i, e_j);
+    List<BigInteger> labelRepresentatives = new ArrayList<>();
+    labelRepresentatives.add(BigInteger.valueOf(6113));
+    gsEdge.setLabelRepresentatives(labelRepresentatives);
+    assertNotNull(gsEdge.getLabelRepresentatives());
+    assertEquals(BigInteger.valueOf(6113), gsEdge.getLabelRepresentatives().get(0));
+  }
+
+  @Test
+  void setLabelRepresentatives() {
+    gsEdge = new GSEdge(e_i, e_j);
+    List<BigInteger> labelRepresentatives = new ArrayList<>();
+    labelRepresentatives.add(BigInteger.valueOf(7243));
+    gsEdge.setLabelRepresentatives(labelRepresentatives);
+    assertNotNull(gsEdge.getLabelRepresentatives());
+    assertEquals(BigInteger.valueOf(7243), gsEdge.getLabelRepresentatives().get(0));
+  }
+
+  @Test
+  void getLabels() {
+    gsEdge = new GSEdge(e_i, e_j);
+    List<String> labels = new ArrayList<>();
+    labels.add("UK");
+    gsEdge.setLabels(labels);
+    assertNotNull(gsEdge.getLabels());
+    assertEquals("UK", gsEdge.getLabels().get(0));
+  }
+}
