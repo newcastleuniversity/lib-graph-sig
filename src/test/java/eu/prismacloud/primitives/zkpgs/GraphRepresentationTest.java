61b6afc9854961ca23a2718befb9f2309db616e2
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-07-14 20:30:43 +0100

    updated test for graph representation

--- src/test/java/eu/prismacloud/primitives/zkpgs/GraphRepresentationTest.java
+++ src/test/java/eu/prismacloud/primitives/zkpgs/GraphRepresentationTest.java
@@ -57,29 +57,17 @@ class GraphRepresentationTest {
 
   @BeforeEach
   void setUp() throws ImportException {
-
     File file = GraphMLProvider.getGraphMLFile(SIGNER_GRAPH_FILE);
-    assertNotNull(file);
-
     graph = new DefaultUndirectedGraph<GSVertex, GSEdge>(GSEdge.class);
     GraphImporter<GSVertex, GSEdge> importer = GraphMLProvider.createImporter();
-    assertNotNull(importer);
-
     importer.importGraph(graph, file);
-    assertNotNull(graph);
-
     graphEncodingParameters = new GraphEncodingParameters(100, 120, 500, 256, 16);
     gsGraph = new GSGraph<>(graph);
-
     graphi = gsGraph.createGraph(SIGNER_GRAPH_FILE);
-    assertNotNull(gsGraph);
-
-    gsGraph.encodeGraph(graphi, graphEncodingParameters);
   }
 
   @Test
   void encodeGraph() {
-    /** TODO fix test for graph representation */
     GraphRepresentation graphRepresentation = new GraphRepresentation();
 
     graphRepresentation.encode(gsGraph, graphEncodingParameters, extendedPublicKey);
