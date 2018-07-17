c7d09566f00c57d316f3f45bee7e0ac03296ffb1
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-07-17 11:26:47 +0100

    add support for serialization for GSMessage

--- src/main/java/eu/prismacloud/primitives/zkpgs/message/GSMessage.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/message/GSMessage.java
@@ -1,16 +1,19 @@
 package eu.prismacloud.primitives.zkpgs.message;
 
 import eu.prismacloud.primitives.zkpgs.commitment.GSCommitment;
-import eu.prismacloud.primitives.zkpgs.commitment.ICommitment;
 import eu.prismacloud.primitives.zkpgs.signature.GSSignature;
 import eu.prismacloud.primitives.zkpgs.util.URN;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GSMessage<T> implements IMessage {
+public class GSMessage implements Serializable {
+
+  private static final long serialVersionUID = -8931520272759188134L;
 
   /** TODO finish gsmessage class to use a proxy */
   Map<URN, Object> messageElements = new HashMap<>();
+
   private GSSignature graphSignature;
   private GSCommitment gsCommitment;
 
@@ -20,13 +23,6 @@ public class GSMessage<T> implements IMessage {
     this.messageElements = messageElements;
   }
 
-  public GSMessage(Object messageElement) {
-    //    this.messageElements.put(, )
-  }
-
-  public void sendTo(Object receiver, T message) {
-    /** TODO add implementation for sendTo method */
-  }
 
   public Map<URN, Object> getMessageElements() {
     return this.messageElements;
@@ -38,7 +34,6 @@ public class GSMessage<T> implements IMessage {
     return msg;
   }
 
-  @Override
   public GSCommitment getCommitment() {
     return gsCommitment;
   }
