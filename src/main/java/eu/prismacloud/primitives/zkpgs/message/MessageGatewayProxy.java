7b15fbb255868edfabd45b25414fa1f976bf7345
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-07-19 17:31:56 +0100

    refactor MessageGatewayProxy
    add javadocs

--- src/main/java/eu/prismacloud/primitives/zkpgs/message/MessageGatewayProxy.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/message/MessageGatewayProxy.java
@@ -1,26 +1,40 @@
 package eu.prismacloud.primitives.zkpgs.message;
 
-import eu.prismacloud.primitives.zkpgs.util.GSLoggerConfiguration;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/** */
-public class MessageGatewayProxy  {
+/** Creates a message gateway proxy for sending and receiving messages. */
+public class MessageGatewayProxy {
   private IMessageGateway messageGateway;
-  private Logger gslog = GSLoggerConfiguration.getGSlog();
-  public MessageGatewayProxy(String type) {
 
+  /**
+   * Instantiates a new message gateway proxy with a specific implementation. Currently, the socket
+   * interface is supported for creating a blocking socket server and a socket client.
+   *
+   * @param type the type of the message gateway we create
+   */
+  public MessageGatewayProxy(String type) {
     messageGateway = new SocketMessageGatewayImpl(type);
   }
 
+  /**
+   * Sends a message via the message gateway proxy.
+   *
+   * @param message the message to send using the message gateway
+   */
   public void send(GSMessage message) {
-    gslog.log(Level.INFO, "send proxy message" + message);
     messageGateway.send(message);
   }
 
+  /**
+   * Receives a message via the message gateway proxy.
+   *
+   * @return the message to receive from the message gateway proxy.
+   */
   public GSMessage receive() {
     GSMessage message = messageGateway.receive();
-    gslog.log(Level.INFO, "receive proxy message" + message);
     return message;
+  }
+
+  /** Closes the message gateway proxy. */
+  public void close() {
+    messageGateway.close();
   }
 }
