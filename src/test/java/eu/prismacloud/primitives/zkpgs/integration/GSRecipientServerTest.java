953b907c46ac49e645691ae33569d420d4c6cc78
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-08-04 13:56:07 +0100

    update tests to use persisted ExtendedPublicKey for recipient orchestrator

--- src/test/java/eu/prismacloud/primitives/zkpgs/integration/GSRecipientServerTest.java
+++ src/test/java/eu/prismacloud/primitives/zkpgs/integration/GSRecipientServerTest.java
@@ -5,21 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import eu.prismacloud.primitives.zkpgs.BaseTest;
 import eu.prismacloud.primitives.zkpgs.EnabledOnSuite;
 import eu.prismacloud.primitives.zkpgs.GSSuite;
-import eu.prismacloud.primitives.zkpgs.keys.ExtendedKeyPair;
 import eu.prismacloud.primitives.zkpgs.keys.ExtendedPublicKey;
-import eu.prismacloud.primitives.zkpgs.keys.SignerKeyPair;
 import eu.prismacloud.primitives.zkpgs.orchestrator.RecipientOrchestrator;
 import eu.prismacloud.primitives.zkpgs.parameters.GraphEncodingParameters;
 import eu.prismacloud.primitives.zkpgs.parameters.KeyGenParameters;
+import eu.prismacloud.primitives.zkpgs.util.FilePersistenceUtil;
 import eu.prismacloud.primitives.zkpgs.util.GSLoggerConfiguration;
 import java.io.IOException;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 /**
  * Testing the signer side of the Issuing protocol with a 2048 modulus bitlength using a persisted
@@ -28,33 +25,27 @@ import org.junit.jupiter.api.extension.ExtendWith;
 @TestInstance(Lifecycle.PER_CLASS)
 @EnabledOnSuite(name = GSSuite.RECIPIENT_SIGNER)
 public class GSRecipientServerTest {
-  private Logger log = GSLoggerConfiguration.getGSlog();
   private KeyGenParameters keyGenParameters;
   private GraphEncodingParameters graphEncodingParameters;
-  private ExtendedKeyPair extendedKeyPair;
-  private SignerKeyPair signerKeyPair;
   private RecipientOrchestrator recipientOrchestrator;
   private ExtendedPublicKey extendedPublicKey;
+  private FilePersistenceUtil persistenceUtil;
+  private Logger gslog = GSLoggerConfiguration.getGSlog();
 
   @BeforeAll
-  void setup1Key() throws IOException, ClassNotFoundException {
+  void setup1Key() throws IOException, ClassNotFoundException, InterruptedException {
     BaseTest baseTest = new BaseTest();
     baseTest.setup();
-    baseTest.shouldCreateASignerKeyPair(BaseTest.MODULUS_BIT_LENGTH);
-    signerKeyPair = baseTest.getSignerKeyPair();
+    FilePersistenceUtil persistenceUtil = new FilePersistenceUtil();
     graphEncodingParameters = baseTest.getGraphEncodingParameters();
     keyGenParameters = baseTest.getKeyGenParameters();
 
-    extendedKeyPair = new ExtendedKeyPair(signerKeyPair, graphEncodingParameters, keyGenParameters);
-    extendedKeyPair.generateBases();
-    extendedKeyPair.graphEncodingSetup();
-    extendedKeyPair.createExtendedKeyPair();
-
-    extendedPublicKey = extendedKeyPair.getExtendedPublicKey();
-    extendedKeyPair = null;
-    signerKeyPair = null;
+    Thread.sleep(3000);
+    gslog.info("read ExtendedPublicKey...");
+    String extendedPublicKeyFileName = "ExtendedPublicKey-" + keyGenParameters.getL_n() + ".ser";
+    extendedPublicKey = (ExtendedPublicKey) persistenceUtil.read(extendedPublicKeyFileName);
   }
-  
+
   @EnabledOnSuite(name = GSSuite.RECIPIENT_SIGNER)
   @Test
   void test1RecipientSide() throws Exception {
@@ -63,9 +54,7 @@ public class GSRecipientServerTest {
         new RecipientOrchestrator(extendedPublicKey, keyGenParameters, graphEncodingParameters);
 
     recipientOrchestrator.round1();
-
     recipientOrchestrator.round3();
-
     recipientOrchestrator.close();
 
     assertNotNull(extendedPublicKey);
