b71e1364bbe9c49c15a0bf5534e181c1beade537
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-08-04 13:57:30 +0100

    write ExtendedPublickey in GSSignerClientTest

--- src/test/java/eu/prismacloud/primitives/zkpgs/integration/GSSignerClientTest.java
+++ src/test/java/eu/prismacloud/primitives/zkpgs/integration/GSSignerClientTest.java
@@ -10,22 +10,18 @@ import eu.prismacloud.primitives.zkpgs.keys.SignerKeyPair;
 import eu.prismacloud.primitives.zkpgs.orchestrator.SignerOrchestrator;
 import eu.prismacloud.primitives.zkpgs.parameters.GraphEncodingParameters;
 import eu.prismacloud.primitives.zkpgs.parameters.KeyGenParameters;
+import eu.prismacloud.primitives.zkpgs.util.FilePersistenceUtil;
 import eu.prismacloud.primitives.zkpgs.util.GSLoggerConfiguration;
 import java.io.IOException;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.junit.jupiter.api.condition.DisabledIf;
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
 
 /**
- * Testing the recipient side of the Issuing protocol with a 2048 modulus bitlength using a persisted
- * and serialised SignerKeyPair to perform computations.
+ * Testing the recipient side of the Issuing protocol with a 2048 modulus bitlength using a
+ * persisted and serialised SignerKeyPair to perform computations.
  */
 @TestInstance(Lifecycle.PER_CLASS)
 @EnabledOnSuite(name = GSSuite.RECIPIENT_SIGNER)
@@ -36,11 +32,13 @@ public class GSSignerClientTest {
   private ExtendedKeyPair extendedKeyPair;
   private SignerKeyPair gsk;
   private SignerOrchestrator signerOrchestrator;
-  
+  private FilePersistenceUtil persistenceUtil;
+  private Boolean generateKeys = true;
+
   @BeforeAll
   void setup2Key() throws IOException, ClassNotFoundException, InterruptedException {
     Thread.sleep(200);
-    
+    FilePersistenceUtil persistenceUtil = new FilePersistenceUtil();
     BaseTest baseTest = new BaseTest();
     baseTest.setup();
     baseTest.shouldCreateASignerKeyPair(BaseTest.MODULUS_BIT_LENGTH);
@@ -52,11 +50,18 @@ public class GSSignerClientTest {
     extendedKeyPair.generateBases();
     extendedKeyPair.graphEncodingSetup();
     extendedKeyPair.createExtendedKeyPair();
+
+    if (generateKeys) {
+      log.info("Test writeExtendedPublicKey: writing new ExtendedPublicKey...");
+      String extendedPublicKeyFileName = "ExtendedPublicKey-" + keyGenParameters.getL_n() + ".ser";
+      persistenceUtil.write(extendedKeyPair.getExtendedPublicKey(), extendedPublicKeyFileName);
+    }
   }
-  
+
   @EnabledOnSuite(name = GSSuite.RECIPIENT_SIGNER)
   @Test
   void test2SignerSide() throws Exception {
+    Thread.sleep(3000);
     signerOrchestrator =
         new SignerOrchestrator(extendedKeyPair, keyGenParameters, graphEncodingParameters);
 
