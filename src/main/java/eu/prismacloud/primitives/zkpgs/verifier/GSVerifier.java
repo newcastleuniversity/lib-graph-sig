87ff37d24ae9f21ce1b1e482db1d8105cfa39942
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-08-08 17:04:14 +0100

    update GSVerifier with suppor for messaging

--- src/main/java/eu/prismacloud/primitives/zkpgs/verifier/GSVerifier.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/verifier/GSVerifier.java
@@ -1,5 +1,7 @@
 package eu.prismacloud.primitives.zkpgs.verifier;
 
+import eu.prismacloud.primitives.zkpgs.message.GSMessage;
+import eu.prismacloud.primitives.zkpgs.message.MessageGatewayProxy;
 import eu.prismacloud.primitives.zkpgs.parameters.KeyGenParameters;
 import eu.prismacloud.primitives.zkpgs.prover.ProofSignature;
 import eu.prismacloud.primitives.zkpgs.store.ProofStore;
@@ -8,15 +10,17 @@ import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GSVerifier  {
-
+public class GSVerifier {
   private final Map<URN, BigInteger> barV = new HashMap<>();
   private final ProofStore<Object> verifierStore;
   private final KeyGenParameters keyGenParameters;
+  private static final String CLIENT = "client";
+  private final MessageGatewayProxy messageGateway;
 
   public GSVerifier(ProofStore<Object> verifierStore, KeyGenParameters keyGenParameters) {
     this.verifierStore = verifierStore;
     this.keyGenParameters = keyGenParameters;
+    this.messageGateway = new MessageGatewayProxy(CLIENT);
   }
 
   public Map<URN, BigInteger> getBarV() {
@@ -31,5 +35,17 @@ public class GSVerifier  {
             + 1;
     int hatvLength =
         keyGenParameters.getL_v() + keyGenParameters.getL_statzk() + keyGenParameters.getL_H() + 1;
+  }
+
+  public void sendMessage(GSMessage messageToProver) {
+    messageGateway.send(messageToProver);
+  }
+
+  public GSMessage receiveMessage() {
+    return messageGateway.receive();
+  }
+
+  public void close() {
+    messageGateway.close();
   }
 }
