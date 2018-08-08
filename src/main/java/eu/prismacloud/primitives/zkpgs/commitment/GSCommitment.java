cbde490ddc37bbef2e0e939550e30431f11c9e69
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-08-08 17:14:21 +0100

    fix typo

--- src/main/java/eu/prismacloud/primitives/zkpgs/commitment/GSCommitment.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/commitment/GSCommitment.java
@@ -11,7 +11,7 @@ public class GSCommitment implements Serializable {
 
   private static final long serialVersionUID = -6253701534775989050L;
   private BigInteger m_i;
-  private BigInteger r_i1;
+  private BigInteger r_i;
   private GroupElement baseS;
   private GroupElement R_i;
 
@@ -46,34 +46,31 @@ public class GSCommitment implements Serializable {
   }
 
   public GSCommitment(
-      GroupElement R_i, BigInteger m_i, BigInteger r_i1, GroupElement baseS, BigInteger modN) {
+      GroupElement R_i, BigInteger m_i, BigInteger r_i, GroupElement baseS, BigInteger modN) {
 
     this.R_i = R_i;
     this.m_i = m_i;
-    this.r_i1 = r_i1;
+    this.r_i = r_i;
     this.baseS = baseS;
     this.modN = modN;
   }
 
-  public GSCommitment(GroupElement commitmentValue){
+  public GSCommitment(GroupElement commitmentValue) {
     this.commitmentValue = commitmentValue;
   }
-  
 
-//  public BigInteger commit() {
-//
-//    BigInteger baseResult = CryptoUtilsFacade.computeMultiBaseExMap(basesR, exponents, modN);
-//
-//    commitmentValue = baseResult.multiply(elementBaseS.modPow(randomness, modN).getValue());
-//
-//    return commitmentValue;
-//  }
+  //  public BigInteger commit() {
+  //
+  //    BigInteger baseResult = CryptoUtilsFacade.computeMultiBaseExMap(basesR, exponents, modN);
+  //
+  //    commitmentValue = baseResult.multiply(elementBaseS.modPow(randomness, modN).getValue());
+  //
+  //    return commitmentValue;
+  //  }
 
   public void setCommitmentValue(GroupElement commitmentValue) {
     this.commitmentValue = commitmentValue;
   }
-
-
 
   public GroupElement getCommitmentValue() {
     return commitmentValue;
@@ -91,80 +88,80 @@ public class GSCommitment implements Serializable {
     return randomness;
   }
 
-//  @Override
-//  public boolean equals(Object o) {
-//    if (this == o) {
-//      return true;
-//    }
-//    if (o == null || this.getClass() != o.getClass()) {
-//      return false;
-//    }
-//
-//    GSCommitment that = (GSCommitment) o;
-//
-//    if (this.m_i != null ? !this.m_i.equals(that.m_i) : that.m_i != null) {
-//      return false;
-//    }
-//    if (this.r_i1 != null ? !this.r_i1.equals(that.r_i1) : that.r_i1 != null) {
-//      return false;
-//    }
-//    if (this.baseS != null ? !this.baseS.equals(that.baseS) : that.baseS != null) {
-//      return false;
-//    }
-//    if (this.R_i != null ? !this.R_i.equals(that.R_i) : that.R_i != null) {
-//      return false;
-//    }
-//    if (this.getCommitmentValue() != null
-//        ? !this.getCommitmentValue().equals(that.getCommitmentValue())
-//        : that.getCommitmentValue() != null) {
-//      return false;
-//    }
-//    if (this.getBasesR() != null
-//        ? !this.getBasesR().equals(that.getBasesR())
-//        : that.getBasesR() != null) {
-//      return false;
-//    }
-//    if (this.getExponents() != null
-//        ? !this.getExponents().equals(that.getExponents())
-//        : that.getExponents() != null) {
-//      return false;
-//    }
-//    if (this.getRandomness() != null
-//        ? !this.getRandomness().equals(that.getRandomness())
-//        : that.getRandomness() != null) {
-//      return false;
-//    }
-//    if (this.elementBaseS != null
-//        ? !this.elementBaseS.equals(that.elementBaseS)
-//        : that.elementBaseS != null) {
-//      return false;
-//    }
-//    return this.modN != null ? this.modN.equals(that.modN) : that.modN == null;
-//  }
-//
-//  @Override
-//  public int hashCode() {
-//    int result = this.m_i != null ? this.m_i.hashCode() : 0;
-//    result = 31 * result + (this.r_i1 != null ? this.r_i1.hashCode() : 0);
-//    result = 31 * result + (this.baseS != null ? this.baseS.hashCode() : 0);
-//    result = 31 * result + (this.R_i != null ? this.R_i.hashCode() : 0);
-//    result =
-//        31 * result
-//            + (this.getCommitmentValue() != null ? this.getCommitmentValue().hashCode() : 0);
-//    result = 31 * result + (this.getBasesR() != null ? this.getBasesR().hashCode() : 0);
-//    result = 31 * result + (this.getExponents() != null ? this.getExponents().hashCode() : 0);
-//    result = 31 * result + (this.getRandomness() != null ? this.getRandomness().hashCode() : 0);
-//    result = 31 * result + (this.elementBaseS != null ? this.elementBaseS.hashCode() : 0);
-//    result = 31 * result + (this.modN != null ? this.modN.hashCode() : 0);
-//    return result;
-//  }
+  //  @Override
+  //  public boolean equals(Object o) {
+  //    if (this == o) {
+  //      return true;
+  //    }
+  //    if (o == null || this.getClass() != o.getClass()) {
+  //      return false;
+  //    }
+  //
+  //    GSCommitment that = (GSCommitment) o;
+  //
+  //    if (this.m_i != null ? !this.m_i.equals(that.m_i) : that.m_i != null) {
+  //      return false;
+  //    }
+  //    if (this.r_i != null ? !this.r_i.equals(that.r_i) : that.r_i != null) {
+  //      return false;
+  //    }
+  //    if (this.baseS != null ? !this.baseS.equals(that.baseS) : that.baseS != null) {
+  //      return false;
+  //    }
+  //    if (this.R_i != null ? !this.R_i.equals(that.R_i) : that.R_i != null) {
+  //      return false;
+  //    }
+  //    if (this.getCommitmentValue() != null
+  //        ? !this.getCommitmentValue().equals(that.getCommitmentValue())
+  //        : that.getCommitmentValue() != null) {
+  //      return false;
+  //    }
+  //    if (this.getBasesR() != null
+  //        ? !this.getBasesR().equals(that.getBasesR())
+  //        : that.getBasesR() != null) {
+  //      return false;
+  //    }
+  //    if (this.getExponents() != null
+  //        ? !this.getExponents().equals(that.getExponents())
+  //        : that.getExponents() != null) {
+  //      return false;
+  //    }
+  //    if (this.getRandomness() != null
+  //        ? !this.getRandomness().equals(that.getRandomness())
+  //        : that.getRandomness() != null) {
+  //      return false;
+  //    }
+  //    if (this.elementBaseS != null
+  //        ? !this.elementBaseS.equals(that.elementBaseS)
+  //        : that.elementBaseS != null) {
+  //      return false;
+  //    }
+  //    return this.modN != null ? this.modN.equals(that.modN) : that.modN == null;
+  //  }
+  //
+  //  @Override
+  //  public int hashCode() {
+  //    int result = this.m_i != null ? this.m_i.hashCode() : 0;
+  //    result = 31 * result + (this.r_i != null ? this.r_i.hashCode() : 0);
+  //    result = 31 * result + (this.baseS != null ? this.baseS.hashCode() : 0);
+  //    result = 31 * result + (this.R_i != null ? this.R_i.hashCode() : 0);
+  //    result =
+  //        31 * result
+  //            + (this.getCommitmentValue() != null ? this.getCommitmentValue().hashCode() : 0);
+  //    result = 31 * result + (this.getBasesR() != null ? this.getBasesR().hashCode() : 0);
+  //    result = 31 * result + (this.getExponents() != null ? this.getExponents().hashCode() : 0);
+  //    result = 31 * result + (this.getRandomness() != null ? this.getRandomness().hashCode() : 0);
+  //    result = 31 * result + (this.elementBaseS != null ? this.elementBaseS.hashCode() : 0);
+  //    result = 31 * result + (this.modN != null ? this.modN.hashCode() : 0);
+  //    return result;
+  //  }
 
   @Override
   public String toString() {
     final StringBuilder sb =
         new StringBuilder("eu.prismacloud.primitives.zkpgs.commitment.GSCommitment{");
     sb.append("m_i=").append(m_i);
-    sb.append(", r_i1=").append(r_i1);
+    sb.append(", r_i=").append(r_i);
     sb.append(", baseS=").append(baseS);
     sb.append(", R_i=").append(R_i);
     sb.append(", commitmentValue=").append(commitmentValue);
@@ -173,7 +170,7 @@ public class GSCommitment implements Serializable {
     sb.append(", randomness=").append(randomness);
     sb.append(", elementBaseS=").append(elementBaseS);
     sb.append(", modN=").append(modN);
-//    sb.append(", commit=").append(commit());
+    //    sb.append(", commit=").append(commit());
     sb.append('}');
     return sb.toString();
   }
