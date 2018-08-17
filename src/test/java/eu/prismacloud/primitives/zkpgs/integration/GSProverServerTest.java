a92b8f6ca9e11c63b27225ac2d8e623337e62171
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-08-17 16:28:02 +0300

    use the signing oracle to generate a fresh signature for the Prover

--- src/test/java/eu/prismacloud/primitives/zkpgs/integration/GSProverServerTest.java
+++ src/test/java/eu/prismacloud/primitives/zkpgs/integration/GSProverServerTest.java
@@ -1,8 +1,5 @@
 package eu.prismacloud.primitives.zkpgs.integration;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import eu.prismacloud.primitives.zkpgs.BaseRepresentation;
 import eu.prismacloud.primitives.zkpgs.BaseRepresentation.BASE;
 import eu.prismacloud.primitives.zkpgs.BaseTest;
@@ -18,6 +15,7 @@ import eu.prismacloud.primitives.zkpgs.orchestrator.ProverOrchestrator;
 import eu.prismacloud.primitives.zkpgs.parameters.GraphEncodingParameters;
 import eu.prismacloud.primitives.zkpgs.parameters.KeyGenParameters;
 import eu.prismacloud.primitives.zkpgs.signature.GSSignature;
+import eu.prismacloud.primitives.zkpgs.signer.GSSigningOracle;
 import eu.prismacloud.primitives.zkpgs.store.ProofStore;
 import eu.prismacloud.primitives.zkpgs.util.BaseCollection;
 import eu.prismacloud.primitives.zkpgs.util.BaseCollectionImpl;
@@ -25,7 +23,6 @@ import eu.prismacloud.primitives.zkpgs.util.CryptoUtilsFacade;
 import eu.prismacloud.primitives.zkpgs.util.FilePersistenceUtil;
 import eu.prismacloud.primitives.zkpgs.util.GSLoggerConfiguration;
 import eu.prismacloud.primitives.zkpgs.util.crypto.GroupElement;
-import eu.prismacloud.primitives.zkpgs.util.crypto.QRGroupPQ;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Iterator;
@@ -58,6 +55,8 @@ public class GSProverServerTest {
   private GSCommitment commitment;
   private Iterator<BaseRepresentation> vertexIterator;
   private ProofStore<Object> proofStore;
+  private GSSigningOracle oracle;
+  private GSSignature sigmaM;
 
   @BeforeAll
   void setupKey()
@@ -78,20 +77,27 @@ public class GSProverServerTest {
     extendedPublicKey = (ExtendedPublicKey) persistenceUtil.read(extendedPublicKeyFileName);
     publicKey = extendedPublicKey.getPublicKey();
     gslog.info("read persisted graph signature");
-    A = (GroupElement) persistenceUtil.read("A.ser");
-    e = (BigInteger) persistenceUtil.read("e.ser");
-    v = (BigInteger) persistenceUtil.read("v.ser");
+
+    oracle = new GSSigningOracle(signerKeyPair, keyGenParameters);
+
+    //    A = (GroupElement) persistenceUtil.read("A.ser");
+    //    e = (BigInteger) persistenceUtil.read("e.ser");
+    //    v = (BigInteger) persistenceUtil.read("v.ser");
 
     gslog.info("read encoded base collection");
     baseCollection = (BaseCollection) persistenceUtil.read("baseCollection.ser");
     vertexIterator = baseCollection.createIterator(BASE.VERTEX).iterator();
     proofStore = new ProofStore<>();
-    generateTestSignature(proofStore);
   }
 
   @EnabledOnSuite(name = GSSuite.PROVER_VERIFIER)
   @Test
   void testProverSide() throws Exception {
+
+    m_0 = CryptoUtilsFacade.computeRandomNumber(keyGenParameters.getL_m());
+    proofStore.store("recipient.m_0", m_0);
+    sigmaM = oracle.sign(m_0);
+    storeGS(sigmaM);
 
     BaseRepresentation baseR0 =
         new BaseRepresentation(extendedPublicKey.getPublicKey().getBaseR_0(), -1, BASE.BASE0);
@@ -117,45 +123,17 @@ public class GSProverServerTest {
     proverOrchestrator.close();
   }
 
-  void generateTestSignature(ProofStore<Object> proofStore) throws ProofStoreException {
-    BigInteger modN = publicKey.getModN();
-    m_0 = CryptoUtilsFacade.computeRandomNumber(keyGenParameters.getL_m());
-    proofStore.store("recipient.m_0", m_0);
+  private void storeGS(GSSignature sigma) throws Exception {
+    String gsURN = "graphsignature";
+    proofStore.store(gsURN, sigma);
 
-    GroupElement baseS = publicKey.getBaseS();
-    GroupElement baseZ = publicKey.getBaseZ();
-    GroupElement R_0 = publicKey.getBaseR_0();
-    QRGroupPQ qrGroupPQ = (QRGroupPQ) publicKey.getQRGroup();
+    String AURN = "graphsignature.A";
+    proofStore.store(AURN, sigma.getA());
 
-    BigInteger vbar = CryptoUtilsFacade.computeRandomNumberMinusPlus(keyGenParameters.getL_v() - 1);
-    GroupElement R_0com = R_0.modPow(m_0);
-    GroupElement baseScom = baseS.modPow(vbar);
-    GroupElement commitmentValue = R_0com.multiply(baseScom);
-    commitment = new GSCommitment(R_0, m_0, vbar, baseS, modN);
+    String eURN = "graphsignature.e";
+    proofStore.store(eURN, sigma.getE());
 
-    e =
-        CryptoUtilsFacade.computePrimeInRange(
-            keyGenParameters.getLowerBoundE(), keyGenParameters.getUpperBoundE());
-
-    BigInteger vPrimePrime =
-        CryptoUtilsFacade.computePrimeInRange(
-            keyGenParameters.getLowerBoundV(), keyGenParameters.getUpperBoundV());
-
-    GroupElement Sv = baseS.modPow(vPrimePrime);
-    GroupElement Sv1 = (Sv.multiply(commitmentValue));
-    GroupElement Q = (baseZ.multiply(Sv1.modInverse()));
-
-    BigInteger order = privateKey.getpPrime().multiply(privateKey.getqPrime());
-    BigInteger d = e.modInverse(order);
-    A = Q.modPow(d);
-    GroupElement sigma = A.modPow(e);
-    assertEquals(sigma, Q, "Signature A not reverting to Q.");
-
-    gsSignature = new GSSignature(signerKeyPair.getPublicKey(), A, e, vPrimePrime);
-    assertTrue(gsSignature.verify(signerKeyPair.getPublicKey(), commitmentValue));
-
-    proofStore.store("graphsignature.A", gsSignature.getA());
-    proofStore.store("graphsignature.e", gsSignature.getE());
-    proofStore.store("graphsignature.v", gsSignature.getV());
+    String vPrimeURN = "graphsignature.v";
+    proofStore.store(vPrimeURN, sigma.getV());
   }
 }
