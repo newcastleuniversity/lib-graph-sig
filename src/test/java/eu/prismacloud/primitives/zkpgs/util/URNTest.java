83d2ff29425c0e0160abedcf5f85af780853dcf2
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-08-24 01:00:43 +0100

    add tests for URN

--- src/test/java/eu/prismacloud/primitives/zkpgs/util/URNTest.java
+++ src/test/java/eu/prismacloud/primitives/zkpgs/util/URNTest.java
@@ -1,22 +1,40 @@
 package eu.prismacloud.primitives.zkpgs.util;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import eu.prismacloud.primitives.zkpgs.util.NamespaceComponent.Type;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class URNTest {
 
-  @BeforeEach
-  void setUp() {}
-
   @Test
   void createURN() {
-	  fail("Test not implemented yet.");
+    NamespaceComponent ns = NamespaceComponent.fromString("ns", Type.IDENTIFIER);
+
+    NamespaceComponent nss = NamespaceComponent.fromString("test.ns", Type.SPECIFIC_STRING);
+    URN testURN = URN.createURN(ns, nss);
+    assertNotNull(testURN);
   }
 
   @Test
-  void fromURI() {
-	  fail("Test not implemented yet.");
+  @DisplayName("Test creating URN from a string")
+  void testCreateURNWithString() {
+    URN testURN = URN.createURN("ns", "test.ns");
+    assertNotNull(testURN);
+  }
+
+  @Test
+  void getZkpgsNameSpaceIdentifier() {
+    URN testURN = URN.createZkpgsURN("test.ns");
+    assertEquals("zkpgs", URN.getZkpgsNameSpaceIdentifier());
+  }
+
+  @Test
+  void createZkpgsURN() {
+    URN testURN = URN.createZkpgsURN("test.ns");
+    assertNotNull(testURN);
   }
 }
