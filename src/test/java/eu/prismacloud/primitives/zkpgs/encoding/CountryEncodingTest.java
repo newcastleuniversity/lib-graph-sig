a7d49eb42def0e759389ccbe300c0824d0ac7966
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-09-02 20:45:10 +0100

    add tests for CountryEncoding class

---
+++ src/test/java/eu/prismacloud/primitives/zkpgs/encoding/CountryEncodingTest.java
@@ -0,0 +1,122 @@
+package eu.prismacloud.primitives.zkpgs.encoding;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import eu.prismacloud.primitives.zkpgs.util.URN;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Map;
+import javax.json.JsonArray;
+import javax.json.JsonReader;
+import javax.json.JsonString;
+import javax.json.JsonValue;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CountryEncodingTest {
+  private static final String COUNTRY_ENCODING_FILE = "country_encoding.json";
+  private InputStream paramStream;
+  private JsonReader reader;
+  private Map<URN, BigInteger> countriesLabel;
+  private JsonArray jsonArray;
+  private JsonValue country;
+  private JsonString countryCodeJson;
+  private String countryCode;
+  private Jsonb jsonb;
+  private CountryEncoding ce;
+  private ArrayList ces;
+
+  @BeforeEach
+  void setUp() {
+    jsonb = JsonbBuilder.create();
+    paramStream =
+        CountryEncodingTest.class.getClassLoader().getResourceAsStream(COUNTRY_ENCODING_FILE);
+  }
+
+  @Test
+  @DisplayName("Test deserialization from json file holding the country encoding")
+  void testDeSerialization() {
+    ces =
+        jsonb.fromJson(
+            paramStream, new ArrayList<CountryEncoding>() {}.getClass().getGenericSuperclass());
+    assertNotNull(ces);
+    assertTrue(!ces.isEmpty());
+  }
+
+  @Test
+  @DisplayName("Test serialization should produce json representation of country encoding")
+  void testSerialization() {
+    CountryEncoding ce = new CountryEncoding();
+    ce.setCountryCode("AD");
+    ce.setCountryName("Andorra");
+    ce.setPrimeRepresentative(2);
+    String result = jsonb.toJson(ce);
+    assertNotNull(result);
+  }
+
+  @Test
+  @DisplayName("Test adding country code to country encoding")
+  void setCountryCode() {
+    CountryEncoding ce = new CountryEncoding();
+    ce.setCountryCode("AD");
+    ce.setCountryName("Andorra");
+    ce.setPrimeRepresentative(2);
+    assertEquals("AD", ce.getCountryCode());
+  }
+
+  @Test
+  @DisplayName("Test returning country name from country encoding")
+  void getCountryName() {
+    ces =
+        jsonb.fromJson(
+            paramStream, new ArrayList<CountryEncoding>() {}.getClass().getGenericSuperclass());
+    CountryEncoding ce = (CountryEncoding) ces.get(0);
+    assertEquals("Andorra", ce.getCountryName());
+  }
+
+  @Test
+  @DisplayName("Test get country code from json")
+  void getCountryCode() {
+    ces =
+        jsonb.fromJson(
+            paramStream, new ArrayList<CountryEncoding>() {}.getClass().getGenericSuperclass());
+    CountryEncoding ce = (CountryEncoding) ces.get(0);
+    assertEquals("AD", ce.getCountryCode());
+  }
+
+  @Test
+  @DisplayName("Test adding country name to country encoding")
+  void setCountryName() {
+    CountryEncoding ce = new CountryEncoding();
+    ce.setCountryCode("AD");
+    ce.setCountryName("Andorra");
+    ce.setPrimeRepresentative(2);
+    assertEquals("Andorra", ce.getCountryName());
+  }
+
+  @Test
+  @DisplayName("Test returning prime representative")
+  void getPrimeRepresentative() {
+    ces =
+        jsonb.fromJson(
+            paramStream, new ArrayList<CountryEncoding>() {}.getClass().getGenericSuperclass());
+    CountryEncoding ce = (CountryEncoding) ces.get(0);
+    assertEquals(2, ce.getPrimeRepresentative());
+  }
+
+  @Test
+  @DisplayName("Test adding prime representative to country encoding")
+  void setPrimeRepresentative() {
+    CountryEncoding ce = new CountryEncoding();
+    ce.setCountryCode("AD");
+    ce.setCountryName("Andorra");
+    ce.setPrimeRepresentative(2);
+    assertEquals(2, ce.getPrimeRepresentative());
+  }
+}
