520e078ee7f5dac59d4541594020e0bcb7fe792c
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-09-03 16:48:24 +0100

    test if country label representatives are in the correct range

--- src/test/java/eu/prismacloud/primitives/zkpgs/parameters/JsonIsoCountriesTest.java
+++ src/test/java/eu/prismacloud/primitives/zkpgs/parameters/JsonIsoCountriesTest.java
@@ -1,42 +1,48 @@
 package eu.prismacloud.primitives.zkpgs.parameters;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import eu.prismacloud.primitives.zkpgs.util.CryptoUtilsFacade;
 import eu.prismacloud.primitives.zkpgs.util.URN;
 import java.io.InputStream;
 import java.math.BigInteger;
 import java.util.Map;
-import javax.json.JsonArray;
-import javax.json.JsonReader;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class JsonIsoCountriesTest {
   private JsonIsoCountries jsonIsoCountries;
-  private JsonReader reader;
-  private JsonArray jsonArray;
+  private GraphEncodingParameters graphEncodingParameters;
 
   @BeforeEach
   void setUp() {
+    graphEncodingParameters = new GraphEncodingParameters(100, 56, 500, 256, 16);
     jsonIsoCountries = new JsonIsoCountries();
   }
 
   @Test
+  @DisplayName("Test parsing json file that holds the country labels encoding")
   void parseParamFile() {
     InputStream encodingStream = jsonIsoCountries.parseEncodingFile();
     assertNotNull(encodingStream);
   }
 
   @Test
+  @DisplayName("Test returning a map of label prime representatives encoding countries in range")
   void getCountryMap() {
-    Map<URN, BigInteger> countryMap = jsonIsoCountries
-        .getCountryMap();
+    Map<URN, BigInteger> countryMap = jsonIsoCountries.getCountryMap();
     assertNotNull(countryMap);
     assertTrue(!countryMap.isEmpty());
 
+    for (BigInteger labelPrimeRepresentative : countryMap.values()) {
+      assertTrue(labelPrimeRepresentative.isProbablePrime(80));
+      assertTrue(
+          CryptoUtilsFacade.isInRange(
+              labelPrimeRepresentative,
+              graphEncodingParameters.getLeastLabelRepresentative(),
+              graphEncodingParameters.getUpperBoundLabelRepresentatives()));
+    }
   }
-
-
 }
