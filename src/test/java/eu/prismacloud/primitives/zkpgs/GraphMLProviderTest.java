14c0f08527140f821e255bbcac75b750946203fe
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-09-04 02:01:10 +0100

    fix test for graphml file parsing

--- src/test/java/eu/prismacloud/primitives/zkpgs/GraphMLProviderTest.java
+++ src/test/java/eu/prismacloud/primitives/zkpgs/GraphMLProviderTest.java
@@ -1,21 +1,26 @@
 package eu.prismacloud.primitives.zkpgs;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import eu.prismacloud.primitives.zkpgs.graph.GSEdge;
 import eu.prismacloud.primitives.zkpgs.graph.GSVertex;
+import eu.prismacloud.primitives.zkpgs.util.GSLoggerConfiguration;
 import java.io.File;
+import java.util.logging.Logger;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultUndirectedGraph;
 import org.jgrapht.io.GraphImporter;
 import org.jgrapht.io.ImportException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 /** */
 class GraphMLProviderTest {
   private static final String SIGNER_GRAPH_FILE = "signer-infra.graphml";
+  private Logger gslog = GSLoggerConfiguration.getGSlog();
 
   @BeforeEach
   void setUp() {}
@@ -27,6 +32,7 @@ class GraphMLProviderTest {
   }
 
   @Test
+  @RepeatedTest(10)
   void createImporter() throws ImportException {
 
     Graph<GSVertex, GSEdge> graph = new DefaultUndirectedGraph<GSVertex, GSEdge>(GSEdge.class);
@@ -41,5 +47,9 @@ class GraphMLProviderTest {
     assertNotNull(graph);
     assertTrue(!graph.vertexSet().isEmpty());
     assertTrue(!graph.edgeSet().isEmpty());
+    gslog.info("vertex size: " + graph.vertexSet().size());
+    gslog.info("edge size: " + graph.edgeSet().size());
+    assertEquals(15, graph.vertexSet().size());
+    assertEquals(14, graph.edgeSet().size());
   }
 }
