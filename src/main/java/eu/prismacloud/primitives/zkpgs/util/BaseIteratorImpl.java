c0dbc60328af57ccc18d662ba03b49e35130b90a
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-09-11 02:01:40 +0100

    add getBaseByIndex in BaseIterator

--- src/main/java/eu/prismacloud/primitives/zkpgs/util/BaseIteratorImpl.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/util/BaseIteratorImpl.java
@@ -2,74 +2,88 @@ package eu.prismacloud.primitives.zkpgs.util;
 
 import eu.prismacloud.primitives.zkpgs.BaseRepresentation;
 import eu.prismacloud.primitives.zkpgs.BaseRepresentation.BASE;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 class BaseIteratorImpl implements BaseIterator {
 
-  private final BASE type;
-  private final List<BaseRepresentation> listOfBases;
-  private List<BaseRepresentation> filteredBases;
-  private int position;
+    private final BASE type;
+    private final List<BaseRepresentation> listOfBases;
+    private List<BaseRepresentation> filteredBases;
+    private int position;
 
-  public BaseIteratorImpl(final BASE type, final List<BaseRepresentation> bases) {
-    this.type = type;
-    this.listOfBases = bases;
-    this.filteredBases = new ArrayList<BaseRepresentation>();
-  }
+    public BaseIteratorImpl(final BASE type, final List<BaseRepresentation> bases) {
+        this.type = type;
+        this.listOfBases = bases;
+        this.filteredBases = new ArrayList<BaseRepresentation>();
+    }
 
-  @Override
-  // TODO hasNext() Does not account for null exponents.
-  public boolean hasNext() {
-    while (position < listOfBases.size()) {
-      BaseRepresentation ba = listOfBases.get(position);
-      if (ba.getBaseType().equals(type) || type.equals(BASE.ALL) || (ba.getExponent() == null)) {
-        return true;
-      } else {
+    @Override
+    // TODO hasNext() Does not account for null exponents.
+    public boolean hasNext() {
+        while (position < listOfBases.size()) {
+            BaseRepresentation ba = listOfBases.get(position);
+            if (ba.getBaseType().equals(type) || type.equals(BASE.ALL) || (ba.getExponent() == null)) {
+                return true;
+            } else {
+                position++;
+            }
+        }
+        return false;
+    }
+
+    public int size() {
+        if (filteredBases.size() == 0) {
+            return filter().size();
+        } else {
+            return filteredBases.size();
+        }
+    }
+
+    @Override
+    public BaseRepresentation next() {
+        BaseRepresentation ba = listOfBases.get(position);
         position++;
-      }
+        return ba;
     }
-    return false;
-  }
 
-  public int size() {
-    if (filteredBases.size() == 0) {
-      return filter().size();
-    } else {
-      return filteredBases.size();
+    @Override
+    public Iterator<BaseRepresentation> iterator() {
+        filter();
+        return filteredBases.iterator();
     }
-  }
 
-  @Override
-  public BaseRepresentation next() {
-    BaseRepresentation ba = listOfBases.get(position);
-    position++;
-    return ba;
-  }
+    public BaseRepresentation getBaseByIndex(int baseIndex) {
+        filter();
 
-  @Override
-  public Iterator<BaseRepresentation> iterator() {
-    filter();
-    return filteredBases.iterator();
-  }
-
-  private List<BaseRepresentation> filter() {
-
-    if (type.equals(BASE.ALL)) {
-      for (BaseRepresentation baseRepresentation : listOfBases) {
-        if (baseRepresentation.getExponent() != null) {
-          filteredBases.add(baseRepresentation);
+        for (BaseRepresentation filteredBase : filteredBases) {
+            if (baseIndex == filteredBase.getBaseIndex()) {
+                return filteredBase;
+            }
         }
-      }
-    } else {
-      for (BaseRepresentation baseRepresentation : listOfBases) {
-        if (type.equals(baseRepresentation.getBaseType())
-            && baseRepresentation.getExponent() != null) {
-          filteredBases.add(baseRepresentation);
-        }
-      }
+
+        return null;
+
     }
-    return filteredBases;
-  }
+
+    private List<BaseRepresentation> filter() {
+
+        if (type.equals(BASE.ALL)) {
+            for (BaseRepresentation baseRepresentation : listOfBases) {
+                if (baseRepresentation.getExponent() != null) {
+                    filteredBases.add(baseRepresentation);
+                }
+            }
+        } else {
+            for (BaseRepresentation baseRepresentation : listOfBases) {
+                if (type.equals(baseRepresentation.getBaseType())
+                        && baseRepresentation.getExponent() != null) {
+                    filteredBases.add(baseRepresentation);
+                }
+            }
+        }
+        return filteredBases;
+    }
 }
