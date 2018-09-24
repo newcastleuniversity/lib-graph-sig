fb0a5197305b222b5a139f82256a0c8203c0b70d
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-09-25 00:17:19 +0100

    clean up GSMessage

--- src/main/java/eu/prismacloud/primitives/zkpgs/message/GSMessage.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/message/GSMessage.java
@@ -1,7 +1,5 @@
 package eu.prismacloud.primitives.zkpgs.message;
 
-import eu.prismacloud.primitives.zkpgs.commitment.GSCommitment;
-import eu.prismacloud.primitives.zkpgs.signature.GSSignature;
 import eu.prismacloud.primitives.zkpgs.store.URN;
 
 import java.io.Serializable;
@@ -12,13 +10,13 @@ public class GSMessage implements Serializable {
 
 	private static final long serialVersionUID = -8931520272759188134L;
 
-	/** TODO finish gsmessage class to use a proxy */
-	Map<URN, Object> messageElements = new HashMap<>();
+	/**
+	 * TODO finish gsmessage class to use a proxy
+	 */
+	Map<URN, Object> messageElements = new HashMap<URN, Object>();
 
-	private GSSignature graphSignature;
-	private GSCommitment gsCommitment;
-
-	public GSMessage() {}
+	public GSMessage() {
+	}
 
 	public GSMessage(Map<URN, Object> messageElements) {
 		this.messageElements = messageElements;
@@ -27,27 +25,5 @@ public class GSMessage implements Serializable {
 
 	public Map<URN, Object> getMessageElements() {
 		return this.messageElements;
-	}
-
-	public void addCommitment(GSCommitment recipientCommitment) {}
-
-	public GSMessage receive(GSMessage msg) {
-		return msg;
-	}
-
-	public GSCommitment getCommitment() {
-		return gsCommitment;
-	}
-
-	public void setCommitment(GSCommitment com) {
-		this.gsCommitment = com;
-	}
-
-	public void addSignature(GSSignature partialGSignature) {
-		this.graphSignature = partialGSignature;
-	}
-
-	public GSSignature getSignature() {
-		return graphSignature;
 	}
 }
