473e5714c6678dbf95d6d73dfc5456a14a0f71e7
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-10-03 13:10:24 +0100

    add default values for http server and contexts

--- src/main/java/eu/prismacloud/primitives/zkpgs/DefaultValues.java
+++ src/main/java/eu/prismacloud/primitives/zkpgs/DefaultValues.java
@@ -6,4 +6,11 @@ public class DefaultValues {
 	public static final String COUNTRY_ENCODING_FILE = "country_encoding.json";
 	public static final String SERVER = "server";
 	public static final String CLIENT = "client";
+	public static final String SERVER_ADDR = "127.0.0.1";
+	public static final int PORT = 9999;
+	public static final String APPLICATION_JSON = "application/json";
+	public static final String CONTENT_TYPE = "Content-Type";
+	public static final String ROOT_CONTEXT = "/";
+	public static final String PROOF_SIGNATURE_CONTEXT = "/proof_signature";
+	public static final String PROOF_REQUEST_CONTEXT = "/proof_request";
 }
