ecc92ca73b9bbf8c9a2e18134682d54c0b05fca2
Ioannis Sfyrakis <ioannis.sfyrakis@newcastle.ac.uk>
2022-01-24 10:28:33 +0000

    Add test case for square hashing with random input
    - use facade for crypto utils to compute random number
    - add display name to test cases to show the intent of the test case

--- src/test/java/uk/ac/ncl/cascade/hashToPrime/SquareHashingTest.java
+++ src/test/java/uk/ac/ncl/cascade/hashToPrime/SquareHashingTest.java
@@ -1,74 +1,90 @@
 package uk.ac.ncl.cascade.hashToPrime;
 
 import eu.prismacloud.primitives.zkpgs.parameters.KeyGenParameters;
+import eu.prismacloud.primitives.zkpgs.util.CryptoUtilsFacade;
 import eu.prismacloud.primitives.zkpgs.util.GSLoggerConfiguration;
 import eu.prismacloud.primitives.zkpgs.util.GSUtils;
+import eu.prismacloud.primitives.zkpgs.util.crypto.SafePrime;
 import eu.prismacloud.primitives.zkpgs.util.crypto.SpecialRSAMod;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 import java.util.logging.Logger;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class SquareHashingTest {
-
-	private static final GSUtils gsUtil = new GSUtils();
 	private SpecialRSAMod safePrime;
-	private static Logger log = GSLoggerConfiguration.getGSlog();
-	private KeyGenParameters keyGenParameters;
-	private SquareHashing squareHash;
+	private static final Logger log = GSLoggerConfiguration.getGSlog();
 	private BigInteger p;
-	private BigInteger q;
-	private BigInteger message;
-	private BigInteger z;
-	private BigInteger b;
-	private BigInteger result;
 	private static final int NUMBER_LENGTH = 512;
 	private static final String N_G = "23998E2A7765B6C913C0ED47D9CB3AC03DB4597D1C4438D61C9FD3418F3D78FFADC59E451FE25A28DD91CEDC59E40980BAE8A176EBEECE412F13466862BFFC3077BB9D26FEB8244ACD4B8D8C868E0095E6AC4122B148FE6F398073111DDCAB8194531CFA8D487B70223CF750E653190732F8BA2A2F7D2BFE2ED175A936BBC7671FC0BB9E45276F81A527F06ABBCC0AFFEDC994BF66D9EB69CC7B61F691FFAB1F78BC6E890A92E332E49519056F502F07206E69E6C182B135D785101DCA408E4F484768854CEAFA0C76355F47";
 	private SpecialRSAMod rsaMod;
 
 	@BeforeEach
 	void setUp() {
-		keyGenParameters = KeyGenParameters.createKeyGenParameters(NUMBER_LENGTH, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0);
-		rsaMod = gsUtil.generateSpecialRSAModulus();
+		KeyGenParameters keyGenParameters = KeyGenParameters.createKeyGenParameters(NUMBER_LENGTH, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0);
+		SafePrime safePrime = CryptoUtilsFacade.computeRandomSafePrime(keyGenParameters);
 
-		p = rsaMod.getP();
-		q = rsaMod.getQ();
+		p = safePrime.getSafePrime();
+		BigInteger q = safePrime.getSophieGermain();
 		log.info("safe prime p: " + p);
 		log.info("safe prime p length: " + p.bitLength());
 		log.info("safe prime q: " + q);
 		log.info("safe prime q length: " + q.bitLength());
-		assertTrue(gsUtil.isPrime(p));
-		assertTrue(gsUtil.isPrime(q));
+		assertTrue(CryptoUtilsFacade.isPrime(p));
+		assertTrue(CryptoUtilsFacade.isPrime(q));
 
 	}
-
-	@AfterEach
-	void tearDown() {
-	}
-
 
 	@Test
-	void hash() {
-		message = new BigInteger(N_G, 16);
+	@DisplayName("Test square hash method with a fixed NG value")
+	void hashNG() {
+		BigInteger message = new BigInteger(N_G, 16);
 		log.info("message: " + message);
 		log.info("message length: " + message.bitLength());
-		z = gsUtil.createRandomNumber(p.bitLength());
+		BigInteger z =  CryptoUtilsFacade.computeRandomNumber(p.bitLength());
 		log.info("z: " + z);
 		log.info("z length: " + z.bitLength());
 
-		b = gsUtil.createRandomNumber(p.bitLength());
+		BigInteger b =  CryptoUtilsFacade.computeRandomNumber(p.bitLength());
 		log.info("b: " + b);
 		log.info("b length: " + b.bitLength());
-		squareHash = new SquareHashing(p, z, b);
+		SquareHashing squareHash = new SquareHashing(p, z, b);
 		assertNotNull(squareHash);
-		result = squareHash.hash(message);
+		BigInteger result = squareHash.hash(message);
 		assertNotNull(result);
+		squareHash = new SquareHashing(p, z, b);
+		assertEquals(squareHash.hash(message), result);
 		log.info("result: " + result);
 		log.info("result length: " + result.bitLength());
+		log.info("square hash: " + squareHash.hash(message));
 	}
+	@Test
+	@DisplayName("Test square hash method with a random input value")
+	void hash() {
+		BigInteger message = CryptoUtilsFacade.computeRandomNumber(1632);
+
+		log.info("message: " + message);
+		log.info("message length: " + message.bitLength());
+		BigInteger z = CryptoUtilsFacade.computeRandomNumber(p.bitLength());
+		log.info("z: " + z);
+		log.info("z length: " + z.bitLength());
+		BigInteger b = CryptoUtilsFacade.computeRandomNumber(p.bitLength());
+		log.info("b: " + b);
+		log.info("b length: " + b.bitLength());
+		SquareHashing squareHash = new SquareHashing(p, z, b);
+		assertNotNull(squareHash);
+		BigInteger result = squareHash.hash(message);
+		assertNotNull(result);
+		squareHash = new SquareHashing(p, z, b);
+		assertEquals(squareHash.hash(message), result);
+		log.info("result: " + result);
+		log.info("result length: " + result.bitLength());
+		log.info("square hash: " + squareHash.hash(message));
+	}
+
 }
